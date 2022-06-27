9abf44b237bed23d9ef373403eb191c902c50de3
Evgeniy Tretiak <103074600+Evgeniy994@users.noreply.github.com>
2022-06-27 18:20:40 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -89,11 +89,18 @@ class Ticket(models.Model):
         if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
             raise ValidationError(
                 {"seat": [
-                    f"seat number must be in available range: (1, seats_in_row): "
+                    f"seat number must be in available range: "
+                    f"(1, seats_in_row): "
                     f"(1, {self.movie_session.cinema_hall.seats_in_row})"
                 ]}
             )
 
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+    def save(
+            self,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None
+    ):
         self.full_clean()
         return super().save(force_insert, force_update, using, update_fields)
