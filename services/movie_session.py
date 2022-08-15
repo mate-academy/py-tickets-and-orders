a6011da2a149d68c22a94589ce2068676d0529fd
Roman Danik <98835053+DanRoman-code@users.noreply.github.com>
2022-08-15 14:50:54 +0300

    Update movie_session.py

--- services/movie_session.py
+++ services/movie_session.py
@@ -5,25 +5,25 @@ def create_movie_session(movie_show_time,
                          movie_id,
                          cinema_hall_id):
     MovieSession.objects.create(show_time=movie_show_time,
-                                movie_id=movie_id,
-                                cinema_hall_id=cinema_hall_id)
+                                movie_id: int = movie_id,
+                                cinema_hall_id:int = cinema_hall_id)
 
 
-def get_movies_sessions(session_date=None):
+def get_movies_sessions(session_date: str = None):
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
     return queryset
 
 
-def get_movie_session_by_id(movie_session_id):
+def get_movie_session_by_id(movie_session_id: int):
     return MovieSession.objects.get(id=movie_session_id)
 
 
-def update_movie_session(session_id,
+def update_movie_session(session_id: int,
                          show_time=None,
-                         movie_id=None,
-                         cinema_hall_id=None):
+                         movie_id: int = None,
+                         cinema_hall_id: int = None):
     movie_session = MovieSession.objects.get(id=session_id)
     if show_time:
         movie_session.show_time = show_time
@@ -34,11 +34,11 @@ def update_movie_session(session_id,
     movie_session.save()
 
 
-def delete_movie_session_by_id(session_id):
+def delete_movie_session_by_id(session_id: int):
     MovieSession.objects.get(id=session_id).delete()
 
 
-def get_taken_seats(movie_session_id):
+def get_taken_seats(movie_session_id: int):
     movie = MovieSession.objects.get(id=movie_session_id)
     return [{"row": ticket.row, "seat": ticket.seat}
             for ticket in movie.ticket_set.all()]
