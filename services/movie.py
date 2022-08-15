1bf57359c5600aeb5ce983ba699e4344e188bbdf
Roman Danik <98835053+DanRoman-code@users.noreply.github.com>
2022-08-15 14:52:13 +0300

    Update movie.py

--- services/movie.py
+++ services/movie.py
@@ -22,8 +22,8 @@ def get_movie_by_id(movie_id: int):
     return Movie.objects.get(id=movie_id)
 
 
-def create_movie(movie_title,
-                 movie_description,
+def create_movie(movie_title: str,
+                 movie_description: str,
                  genres_ids=None,
                  actors_ids=None):
     with transaction.atomic():
