22edaa929fb9b1039af4847c7d9f832a8e22a940
Oleg Usenko <Oleg911Usenko@gmail.com>
2022-10-03 17:09:44 +0300

    Task Complete v2.0

--- db/models.py
+++ db/models.py
@@ -88,9 +88,9 @@ class Ticket(models.Model):
         ]
 
     def __str__(self):
-        return f'{self.movie_session.movie.title} ' \
-               f'{self.movie_session.show_time} ' \
-               f'(row: {self.row}, seat: {self.seat})'
+        return f"{self.movie_session.movie.title} " \
+               f"{self.movie_session.show_time} " \
+               f"(row: {self.row}, seat: {self.seat})"
 
     def clean(self):
         rows = self.movie_session.cinema_hall.rows
