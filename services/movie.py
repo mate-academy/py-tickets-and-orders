3202c24f7508a99026a8fe58a819ec8ad94fb967
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-10-15 22:34:29 +0300

    'py_ticket_and_orders_Solution_7'

--- services/movie.py
+++ services/movie.py
@@ -8,7 +8,7 @@ from db.models import Movie
 def get_movies(
     genres_ids: list[int] = None,
     actors_ids: list[int] = None,
-    title: str = None
+    title: str = None,
 ) -> QuerySet:
     queryset = Movie.objects.all()
 
@@ -32,7 +32,7 @@ def create_movie(
     movie_title: str,
     movie_description: str,
     genres_ids: list = None,
-    actors_ids: list = None
+    actors_ids: list = None,
 ) -> Movie:
     with transaction.atomic():
         movie = Movie.objects.create(
--- services/movie_session.py
+++ services/movie_session.py
@@ -6,7 +6,7 @@ from db.models import MovieSession, Ticket
 def create_movie_session(
     movie_show_time,
     movie_id: int,
-    cinema_hall_id: int
+    cinema_hall_id: int,
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
@@ -30,7 +30,7 @@ def update_movie_session(
     session_id: int,
     show_time: str = None,
     movie_id: int = None,
-    cinema_hall_id: int = None
+    cinema_hall_id: int = None,
 ) -> None:
     movie_session = MovieSession.objects.get(id=session_id)
     if show_time:
