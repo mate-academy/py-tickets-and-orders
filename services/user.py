054e776488f0d9deb8b25dd0d49de46e5183f465
Alex Parkhomenko <alex.wquels@gmail.com>
2022-10-17 20:48:54 +0300

    Solution

---
+++ services/user.py
@@ -0,0 +1,47 @@
+from django.contrib.auth import get_user_model
+from django.db import transaction
+
+from db.models import User
+
+
+def create_user(
+        username: str,
+        password: str,
+        email: str = None,
+        first_name: str = None,
+        last_name: str = None) -> None:
+    get_user_model().objects.create_user(
+        username=username,
+        password=password,
+        email=email if email else "",
+        first_name=first_name if first_name else "",
+        last_name=last_name if last_name else "",
+    )
+
+
+def get_user(user_id: int) -> User:
+    return get_user_model().objects.get(id=user_id)
+
+
+def update_user(
+        user_id: int,
+        username: str = None,
+        password: str = None,
+        email: str = None,
+        first_name: str = None,
+        last_name: str = None,
+) -> None:
+    with transaction.atomic():
+        user = get_user_model().objects.get(id=user_id)
+        if username:
+            user.username = username
+        if password:
+            user.set_password(password)
+        if email:
+            user.email = email
+        if first_name:
+            user.first_name = first_name
+        if last_name:
+            user.last_name = last_name
+
+        user.save()
