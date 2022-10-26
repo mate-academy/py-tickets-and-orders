d0c002b800837fb52e4eb8e7b2d2c3745c0be5fe
Oleksandr Murynskyi <a.murinskiy@gmail.com>
2022-10-26 15:59:16 +0300

    Change create order

--- services/order.py
+++ services/order.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
 
-from db.models import User, Order, Ticket, MovieSession
+from db.models import Order, Ticket
 
 
 def create_order(
@@ -11,17 +12,17 @@ def create_order(
 ) -> None:
 
     with transaction.atomic():
-        order = Order.objects.create(user=User.objects.get(username=username))
+        new_user = get_user_model().objects.get(username=username)
+        order = Order.objects.create(user=new_user)
 
         if date:
             order.created_at = date
             order.save()
 
         for ticket in tickets:
-            movie_session = ticket["movie_session"]
 
             Ticket.objects.create(
-                movie_session=MovieSession.objects.get(id=movie_session),
+                movie_session_id=ticket["movie_session"],
                 row=ticket["row"],
                 seat=ticket["seat"],
                 order=order,
