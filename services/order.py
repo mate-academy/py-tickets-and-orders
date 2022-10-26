544defaa1a55d3eaa922cc7f232ec1b44fbcda23
Andriy Makarenko <andriy@mate.academy>
2022-10-26 14:38:34 +0300

    Solution movie_session_id and few more

--- services/order.py
+++ services/order.py
@@ -1,9 +1,10 @@
-from typing import Any, List
+from typing import List
 
 from django.contrib.auth import get_user_model
 from django.db import transaction
+from django.db.models import QuerySet
 
-from db.models import MovieSession, Order, Ticket
+from db.models import Order, Ticket
 
 
 def create_order(
@@ -21,17 +22,15 @@ def create_order(
 
         for ticket in tickets:
             Ticket.objects.create(
-                movie_session=MovieSession.objects.get(
-                    id=ticket["movie_session"]
-                ),
+                movie_session_id=ticket["movie_session"],
                 order=order,
                 row=ticket["row"],
                 seat=ticket["seat"],
             )
 
 
-def get_orders(username: str = None) -> Any:
-    queryset = Order.objects.all().order_by("-user")
+def get_orders(username: str = None) -> QuerySet:
+    queryset = Order.objects.order_by("-user")
 
     if username:
         queryset = queryset.filter(user__username=username)
