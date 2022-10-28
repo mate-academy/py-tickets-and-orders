b18b95c16e79bbf85379ca0754c81a9bf24d52fe
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-10-28 18:58:06 +0300

    related name for order added

--- db/models.py
+++ db/models.py
@@ -61,7 +61,9 @@ class MovieSession(models.Model):
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders"
     )
 
     class Meta:
