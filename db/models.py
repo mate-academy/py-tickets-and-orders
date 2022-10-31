bcb62eb7a08b201d7a44c445ccf130c49c830a7b
Nikyxa <kolisova.veronika81@gmail.com>
2022-10-31 12:09:00 +0200

    'Solution'

--- db/models.py
+++ db/models.py
@@ -46,7 +46,7 @@ class CinemaHall(models.Model):
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
     cinema_hall = models.ForeignKey(to=CinemaHall, on_delete=models.CASCADE)
-    movie = models.ForeignKey(to=Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(to=Movie, on_delete=models.CASCADE, related_name="movie_sessions")
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
@@ -73,9 +73,10 @@ class User(AbstractUser):
 class Ticket(models.Model):
     movie_session = models.ForeignKey(
         "MovieSession",
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="tickets"
     )
-    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+    order = models.ForeignKey("Order", on_delete=models.CASCADE, related_name="tickets")
     row = models.IntegerField()
     seat = models.IntegerField()
 
@@ -118,7 +119,7 @@ class Ticket(models.Model):
         constraints = [
             UniqueConstraint(
                 fields=["row", "seat", "movie_session"],
-                name="unique_constraints"
+                name="unique_ticket"
             )
         ]
 
