3309cdb37a11564113b69cec181704c0c6d1e3e5
Liudmyla Kulinchenko <liudmilakulinchenko@gmail.com>
2022-11-01 13:00:06 +0100

    Remove transactions

--- services/user.py
+++ services/user.py
@@ -11,15 +11,13 @@ def create_user(
         first_name: str = None,
         last_name: str = None
 ) -> None:
-
-    with transaction.atomic():
-        get_user_model().objects.create_user(
-            username=username,
-            password=password,
-            email=email or "",
-            first_name=first_name or "",
-            last_name=last_name or ""
-        )
+    get_user_model().objects.create_user(
+        username=username,
+        password=password,
+        email=email or "",
+        first_name=first_name or "",
+        last_name=last_name or ""
+    )
 
 
 def get_user(user_id: int) -> settings.AUTH_USER_MODEL:
@@ -36,16 +34,15 @@ def update_user(
 ) -> None:
     user = get_user(user_id)
 
-    with transaction.atomic():
-        if username:
-            user.username = username
-        if password:
-            user.set_password(password)
-        if email:
-            user.email = email
-        if first_name:
-            user.first_name = first_name
-        if last_name:
-            user.last_name = last_name
+    if username:
+        user.username = username
+    if password:
+        user.set_password(password)
+    if email:
+        user.email = email
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
 
-        user.save()
+    user.save()
