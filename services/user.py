82999654f85be0b9a0410caafeddf5f3eca80907
Pavlo Kapitanets <aixpavlik"gmail.com>
2022-11-02 23:12:24 +0200

    Solution

--- services/user.py
+++ services/user.py
@@ -7,19 +7,19 @@ def create_user(username: str,
                 email: str = None,
                 first_name: str = None,
                 last_name: str = None) -> None:
-    get_user_model().objects.create_user(
+    user = get_user_model().objects.create_user(
         username=username,
         password=password
     )
-    user = get_user_model().objects.get(username=username)
     if email:
         user.email = email
 
     if first_name:
         user.first_name = first_name
 
-    if email:
+    if last_name:
         user.last_name = last_name
+
     user.save()
 
 
@@ -28,12 +28,12 @@ def get_user(user_id: int) -> User:
 
 
 def update_user(
-        user_id: int,
-        username: str = None,
-        password: str = None,
-        email: str = None,
-        first_name: str = None,
-        last_name: str = None
+    user_id: int,
+    username: str = None,
+    password: str = None,
+    email: str = None,
+    first_name: str = None,
+    last_name: str = None
 ) -> None:
     user = get_user(user_id)
 
