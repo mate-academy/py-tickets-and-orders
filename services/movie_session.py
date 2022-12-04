641ccac1b9195a6b1bf1a0b1958fdbe3262c1d5d
Mykola Zakharov <kolyazakharov94@gmail.com>
2022-12-04 15:21:51 +0200

    solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,6 +1,7 @@
 from django.db.models import QuerySet
 
-from db.models import MovieSession
+from typing import List
+from db.models import MovieSession, Ticket
 
 
 def create_movie_session(
@@ -42,3 +43,9 @@ def update_movie_session(
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(movie_session_id: int) -> List[dict]:
+    return list(Ticket.objects.filter(
+        movie_session_id=movie_session_id
+    ).values("row", "seat"))
