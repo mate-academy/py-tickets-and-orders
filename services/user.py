325cf5d851186fdbaacf46258f6baf8371dc3b19
Your Name <y>
2022-12-14 09:56:30 +0200

    Solution added

---
+++ services/user.py
@@ -0,0 +1,47 @@
+from django.contrib.auth import get_user_model
+
+from db.models import User
+
+
+def create_user(
+        username: str,
+        password: str,
+        ** info
+) -> None:
+    get_user_model().objects.create_user(
+        username=username,
+        password=password,
+        **info
+    )
+
+
+def get_user(user_id: int) -> User:
+    return get_user_model().objects.get(id=user_id)
+
+
+def update_user(
+        user_id: int,
+        username: str = None,
+        password: str = False,
+        email: str = None,
+        first_name: str = None,
+        last_name: str = None
+) -> None:
+    user_to_update = get_user_model().objects.get(id=user_id)
+
+    if username:
+        user_to_update.username = username
+
+    if password:
+        user_to_update.set_password(password)
+
+    if email:
+        user_to_update.email = email
+
+    if first_name:
+        user_to_update.first_name = first_name
+
+    if last_name:
+        user_to_update.last_name = last_name
+
+    user_to_update.save()
