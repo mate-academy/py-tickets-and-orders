8e1fde7250e76d01ec66acc7ac9d68ddc41d2bb7
Mykyta Moshchenko <mykytamoshchenko@outlook.com>
2023-01-02 19:28:01 +0100

    Solution

--- settings.py
+++ settings.py
@@ -15,6 +15,8 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "db.User"
+
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "Europe/Kiev"
@@ -24,5 +26,7 @@ USE_I18N = True
 USE_TZ = False
 
 INSTALLED_APPS = [
+    "django.contrib.contenttypes",
+    "django.contrib.auth",
     "db",
 ]
