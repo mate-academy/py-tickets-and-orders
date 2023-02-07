3eb199e51ce2a75b20317e1e290aa99c8d4cd1d9
Anastasiia Tsurkan <shatalova.anastasiia.of@gmail.com>
2023-02-07 11:36:22 +0200

    Solution 2

--- checklist.md
+++ checklist.md
@@ -1,5 +1,8 @@
 # Сheck Your Code Against the Following Points
-## Don't Push db file
+
+## Don't Push db files
+Make sure you don't push db files (files with `.sqlite`, `.db3`, etc. extension).
+
 ## Don't Forget to Add Migrations to your PR
 This is a required for the tests to pass.
 
--- db/models.py
+++ db/models.py
@@ -10,7 +10,7 @@ class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
-        return f"{self.name}"
+        return self.name
 
 
 class Actor(models.Model):
@@ -28,7 +28,7 @@ class Movie(models.Model):
     genres = models.ManyToManyField(to=Genre)
 
     def __str__(self) -> str:
-        return f"{self.title}"
+        return self.title
 
 
 class CinemaHall(models.Model):
@@ -41,7 +41,7 @@ class CinemaHall(models.Model):
         return self.rows * self.seats_in_row
 
     def __str__(self) -> str:
-        return f"{self.name}"
+        return self.name
 
 
 class MovieSession(models.Model):
