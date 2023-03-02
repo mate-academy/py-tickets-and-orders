6634aa532e98eb50958799f2790a69d7be7d63dc
yura.vataschuk <yura.vataschuk@gmail.com>
2023-03-02 12:00:10 +0200

    Fixed

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Optional
 
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
@@ -91,9 +91,11 @@ class Ticket(models.Model):
         ]
 
     def __str__(self) -> str:
-        return f"{self.movie_session.movie.title} " \
-               f"{self.movie_session.show_time} " \
-               f"(row: {self.row}, seat: {self.seat})"
+        return (
+            f"{self.movie_session.movie.title} "
+            f"{self.movie_session.show_time} "
+            f"(row: {self.row}, seat: {self.seat})"
+        )
 
     def clean(self) -> None:
         if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
@@ -120,8 +122,8 @@ class Ticket(models.Model):
             self,
             force_insert: bool = False,
             force_update: bool = False,
-            using: Any = None,
-            update_fields: Any = None
+            using: Optional[Any] = None,
+            update_fields: Optional[Any] = None
     ) -> None:
 
         self.full_clean()
