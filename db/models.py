d96cd6e3a7fa70b47ef0e87f9aa512f66cc2e334
Paul Maslov <maslovp67@gmail.com>
2023-03-03 19:30:06 +0200

    'Solution'

--- db/models.py
+++ db/models.py
@@ -97,7 +97,8 @@ class Ticket(models.Model):
         ]
 
     def __str__(self) -> str:
-        return f"{self.movie_session.movie.title} {self.order.created_at} " \
+        return f"{self.movie_session.movie.title} " \
+               f"{self.movie_session.show_time} " \
                f"(row: {self.row}, seat: {self.seat})"
 
     def clean(self) -> None:
