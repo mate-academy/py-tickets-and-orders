8e87c1a540734705892f45471ed6af572e23db60
Roman Senyshyn <senjaskorp@gmail.com>
2023-03-07 11:46:45 +0200

    1 commit

--- services/movie_session.py
+++ services/movie_session.py
@@ -3,8 +3,13 @@ from django.db.models import QuerySet
 from db.models import MovieSession
 
 
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    movie_session = MovieSession.objects.get(id=movie_session_id)
+    return list(movie_session.ticket_set.all().values("row", "seat"))
+
+
 def create_movie_session(
-    movie_show_time: str, movie_id: int, cinema_hall_id: int
+        movie_show_time: str, movie_id: int, cinema_hall_id: int
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
@@ -15,8 +20,10 @@ def create_movie_session(
 
 def get_movies_sessions(session_date: str = None) -> QuerySet:
     queryset = MovieSession.objects.all()
+
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
+
     return queryset
 
 
@@ -25,18 +32,22 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 
 def update_movie_session(
-    session_id: int,
-    show_time: str = None,
-    movie_id: int = None,
-    cinema_hall_id: int = None,
+        session_id: int,
+        show_time: str = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None,
 ) -> None:
     movie_session = MovieSession.objects.get(id=session_id)
+
     if show_time:
         movie_session.show_time = show_time
+
     if movie_id:
         movie_session.movie_id = movie_id
+
     if cinema_hall_id:
         movie_session.cinema_hall_id = cinema_hall_id
+
     movie_session.save()
 
 
---
+++ services/order.py
@@ -0,0 +1,40 @@
+import datetime
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Order, Ticket
+
+
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: datetime = None
+) -> None:
+    with transaction.atomic():
+        user = get_user_model().objects.get(username=username)
+        order = Order.objects.create(user=user)
+
+        if date:
+            order.created_at = datetime.datetime.strptime(
+                date, "%Y-%m-%d %H:%M"
+            )
+
+        order.save()
+
+        for ticket in tickets:
+            Ticket.objects.create(
+                movie_session_id=ticket["movie_session"],
+                order=order,
+                row=ticket["row"],
+                seat=ticket["seat"]
+            )
+
+
+def get_orders(username: str = None) -> QuerySet:
+    orders = Order.objects.all()
+
+    if username:
+        orders = orders.filter(user__username=username)
+
+    return orders
