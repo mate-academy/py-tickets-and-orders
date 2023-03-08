1136034a3cf8c37791094561c17c3a77bb0618f2
Viacheslav Shcherbak <misternictof@gmail.com>
2023-03-08 09:04:19 +0200

    Fix anotation

--- db/models.py
+++ db/models.py
@@ -84,9 +84,11 @@ class Ticket(models.Model):
     seat = models.IntegerField()
 
     def __str__(self) -> str:
-        return f"{self.movie_session.movie.title} " \
-               f"{self.movie_session.show_time} " \
-               f"(row: {self.row}, seat: {self.seat})"
+        return (
+            f"{self.movie_session.movie.title} "
+            f"{self.movie_session.show_time} "
+            f"(row: {self.row}, seat: {self.seat})"
+        )
 
     class Meta:
         constraints = [
