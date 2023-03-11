38095a3ad2059eb177547fd594d33bb082876934
Oleh Chornyi <ochernyi04@gmail.com>
2023-03-11 12:31:46 +0200

    Solution

---
+++ services/user.py
@@ -0,0 +1,48 @@
+from typing import Optional
+
+from db.models import User
+
+
+def create_user(
+        username: str,
+        password: str,
+        email: Optional[str] = None,
+        first_name: Optional[str] = None,
+        last_name: Optional[str] = None,
+) -> None:
+    User.objects.create_user(
+        username=username,
+        password=password,
+        email=email if email else "",
+        first_name=first_name if first_name else "",
+        last_name=last_name if last_name else ""
+    )
+
+
+def get_user(user_id: int) -> User:
+    return User.objects.get(pk=user_id)
+
+
+def update_user(
+        user_id: int,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        email: Optional[str] = None,
+        first_name: Optional[str] = None,
+        last_name: Optional[str] = None,
+) -> None:
+    user = User.objects.get(pk=user_id)
+    if password:
+        user.set_password(password)
+
+    if username:
+        user.username = username
+    if password:
+        user.set_password(password)
+    if email:
+        user.email = email
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
+    user.save()
