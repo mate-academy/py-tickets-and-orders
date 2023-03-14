93ec7ebfa48fb6314db0d1db999c0a33ef1d454b
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-03-14 14:51:31 +0200

    indentation fixed

--- services/user.py
+++ services/user.py
@@ -14,13 +14,14 @@ def create_user(
         username=username,
         password=password,
     )
+
     if email:
         user.email = email
-
     if first_name:
         user.first_name = first_name
     if last_name:
         user.last_name = last_name
+
     user.save()
 
 
@@ -37,6 +38,7 @@ def update_user(
         last_name: str = None,
 ) -> None:
     user = get_user(user_id)
+
     if username:
         user.username = username
 
