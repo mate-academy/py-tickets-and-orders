547db1793caeb72ced116b378152e7ebb85fe092
Dmytry <donchykd@gmail.com>
2023-03-14 17:40:35 +0200

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,10 +1,10 @@
 from django.db.models import QuerySet
 
-from db.models import MovieSession
+from db.models import MovieSession, Ticket
 
 
 def create_movie_session(
-    movie_show_time: str, movie_id: int, cinema_hall_id: int
+        movie_show_time: str, movie_id: int, cinema_hall_id: int
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
@@ -25,10 +25,10 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 
 def update_movie_session(
-    session_id: int,
-    show_time: str = None,
-    movie_id: int = None,
-    cinema_hall_id: int = None,
+        session_id: int,
+        show_time: str = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None,
 ) -> None:
     movie_session = MovieSession.objects.get(id=session_id)
     if show_time:
@@ -38,6 +38,11 @@ def update_movie_session(
     if cinema_hall_id:
         movie_session.cinema_hall_id = cinema_hall_id
     movie_session.save()
+
+
+def get_taken_seats(movie_session_id: int) -> list:
+    return list(Ticket.objects.filter(movie_session__id=movie_session_id).
+                values("row", "seat"))
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
