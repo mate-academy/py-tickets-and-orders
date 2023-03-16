1923204a78a576d8a83933765714f569a6e7251b
Sergii Machulin <machulinsv@gmail.com>
2023-03-16 22:37:08 +0200

    Update user and movie_session services

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,10 +1,10 @@
 from django.db.models import QuerySet
 
-from db.models import MovieSession
+from db.models import MovieSession, Ticket
 
 
 def create_movie_session(
-    movie_show_time: str, movie_id: int, cinema_hall_id: int
+        movie_show_time: str, movie_id: int, cinema_hall_id: int
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
@@ -25,10 +25,10 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 
 def update_movie_session(
-    session_id: int,
-    show_time: str = None,
-    movie_id: int = None,
-    cinema_hall_id: int = None,
+        session_id: int,
+        show_time: str = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None,
 ) -> None:
     movie_session = MovieSession.objects.get(id=session_id)
     if show_time:
@@ -45,10 +45,8 @@ def delete_movie_session_by_id(session_id: int) -> None:
 
 
 def get_taken_seats(movie_session_id: int) -> list[dict]:
-    movie_session = MovieSession.objects.get(id=movie_session_id)
-    taken_seats = list(
-        movie_session.tickets.order_by(
-            "row", "seat"
-        ).values("row", "seat")
+    return list(
+        Ticket.objects.filter(movie_session_id=movie_session_id)
+        .order_by("row", "seat")
+        .values("row", "seat")
     )
-    return taken_seats
--- services/user.py
+++ services/user.py
@@ -36,7 +36,7 @@ def update_user(
         first_name: str = None,
         last_name: str = None,
 ) -> None:
-    user_to_update = get_user_model().objects.get(id=user_id)
+    user_to_update = get_user(user_id)
 
     if username:
         user_to_update.username = username
