ef759f5e04089d1ad9c6160af7ca7928224efdb0
usrofgh <mailforgimail@gmail.com>
2023-03-16 11:42:11 +0200

    update

--- db/models.py
+++ db/models.py
@@ -101,17 +101,18 @@ class Ticket(models.Model):
         )
 
     def clean(self) -> None:
-        if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
+        hall = self.movie_session.cinema_hall
+        if not (1 <= self.seat <= hall.seats_in_row):
             raise ValidationError({
                 "seat": f"seat number must be in available range: "
                         f"(1, seats_in_row): "
-                        f"(1, {self.movie_session.cinema_hall.seats_in_row})"
+                        f"(1, {hall.seats_in_row})"
             })
 
-        if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
+        if not (1 <= self.row <= hall.rows):
             raise ValidationError({
                 "row": f"row number must be in available range: (1, rows): "
-                       f"(1, {self.movie_session.cinema_hall.rows})",
+                       f"(1, {hall.rows})",
             })
 
     def save(
