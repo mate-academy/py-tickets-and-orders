55761eee95e4d708f11bdb0db9663a168fc70fc5
Natalie Goriela <natalie.goriela@gmail.com>
2023-03-17 13:47:59 +0200

    'Solution'

--- services/movie.py
+++ services/movie.py
@@ -7,8 +7,8 @@ from db.models import Movie
 
 
 def get_movies(
-    genres_ids: Optional[list] = None,
-    actors_ids: Optional[list] = None,
+    genres_ids: Optional[list[int]] = None,
+    actors_ids: Optional[list[int]] = None,
     title: Optional[str] = None
 ) -> QuerySet:
     queryset = Movie.objects.all()
@@ -29,20 +29,20 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
+@transaction.atomic()
 def create_movie(
     movie_title: str,
     movie_description: str,
-    genres_ids: Optional[list] = None,
-    actors_ids: Optional[list] = None,
+    genres_ids: Optional[list[int]] = None,
+    actors_ids: Optional[list[int]] = None,
 ) -> Movie:
-    with transaction.atomic():
-        movie = Movie.objects.create(
-            title=movie_title,
-            description=movie_description,
-        )
-        if genres_ids:
-            movie.genres.set(genres_ids)
-        if actors_ids:
-            movie.actors.set(actors_ids)
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
 
-        return movie
+    return movie
