ed3e5e87dcea05ce9fd219c1adda065cd4394fb5
Artur Tytomyr <art.tytomyr@gmail.com>
2023-03-20 23:12:42 +0200

    revised

--- db/models.py
+++ db/models.py
@@ -25,8 +25,8 @@ class Actor(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    actors = models.ManyToManyField(to=Actor)
-    genres = models.ManyToManyField(to=Genre)
+    actors = models.ManyToManyField(to=Actor, related_name="movies")
+    genres = models.ManyToManyField(to=Genre, related_name="movies")
 
     def __str__(self) -> str:
         return self.title
@@ -52,8 +52,12 @@ class CinemaHall(models.Model):
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    cinema_hall = models.ForeignKey(to=CinemaHall, on_delete=models.CASCADE)
-    movie = models.ForeignKey(to=Movie, on_delete=models.CASCADE)
+    cinema_hall = models.ForeignKey(to=CinemaHall,
+                                    on_delete=models.CASCADE,
+                                    related_name="movies")
+    movie = models.ForeignKey(to=Movie,
+                              on_delete=models.CASCADE,
+                              related_name="movies")
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
@@ -62,7 +66,8 @@ class MovieSession(models.Model):
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
+                             on_delete=models.CASCADE,
+                             related_name="customer")
 
     def __str__(self) -> str:
         return str(self.created_at)
