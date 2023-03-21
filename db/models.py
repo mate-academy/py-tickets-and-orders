fa2765779a54f70bb95f334422871bd47ac82c19
Zenon <zenion@ukr.net>
2023-03-21 12:52:30 +0200

    Fix `clean()`, add `related_name`

--- db/models.py
+++ db/models.py
@@ -23,8 +23,8 @@ class Actor(models.Model):
 class Movie(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     description = models.TextField()
-    actors = models.ManyToManyField(to=Actor)
-    genres = models.ManyToManyField(to=Genre)
+    actors = models.ManyToManyField(Actor)
+    genres = models.ManyToManyField(Genre)
 
     def __str__(self) -> str:
         return self.title
@@ -45,8 +45,14 @@ class CinemaHall(models.Model):
 
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
-    cinema_hall = models.ForeignKey(to=CinemaHall, on_delete=models.CASCADE)
-    movie = models.ForeignKey(to=Movie, on_delete=models.CASCADE)
+    cinema_hall = models.ForeignKey(
+        CinemaHall,
+        on_delete=models.CASCADE,
+        related_name="sessions")
+    movie = models.ForeignKey(
+        Movie,
+        on_delete=models.CASCADE,
+        related_name="sessions")
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
@@ -56,7 +62,8 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="orders"
     )
 
     class Meta:
@@ -67,8 +74,16 @@ class Order(models.Model):
 
 
 class Ticket(models.Model):
-    movie_session = models.ForeignKey(MovieSession, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    movie_session = models.ForeignKey(
+        MovieSession,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
     row = models.IntegerField()
     seat = models.IntegerField()
 
@@ -86,19 +101,20 @@ class Ticket(models.Model):
                 f"(row: {self.row}, seat: {self.seat})")
 
     def clean(self) -> None:
-        if not 1 <= self.row <= self.movie_session.cinema_hall.rows:
+        session_hall = self.movie_session.cinema_hall
+
+        if not 1 <= self.row <= session_hall.rows:
             raise ValidationError({
                 "row": [
                     "row number must be in available range: (1, rows): "
-                    f"(1, {self.movie_session.cinema_hall.rows})"
+                    f"(1, {session_hall.rows})"
                 ]
             })
-        if not 1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row:
+        if not 1 <= self.seat <= session_hall.seats_in_row:
             raise ValidationError({
                 "seat": [
                     "seat number must be in available range: "
-                    "(1, seats_in_row): "
-                    f"(1, {self.movie_session.cinema_hall.seats_in_row})"
+                    f"(1, seats_in_row): (1, {session_hall.seats_in_row})"
                 ]
             })
 
