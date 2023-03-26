307df5db75333267acc55f8a3929292b9d499052
Bohdan2001007 <bogdan1991007@gmail.com>
2023-03-26 16:37:42 +0200

    Solution

--- services/movie.py
+++ services/movie.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from django.db import transaction
 from django.db.models import QuerySet
@@ -7,8 +7,8 @@ from db.models import Movie
 
 
 def get_movies(
-        genres_ids: List[int] = None,
-        actors_ids: List[int] = None,
+        genres_ids: Optional[List[int]] = None,
+        actors_ids: Optional[List[int]] = None,
         title: str = None
 ) -> QuerySet:
     queryset = Movie.objects.all()
