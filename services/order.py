f7a9707488885ec4dbe9e801b7e360fac182d7a4
ViktorKr <kravchukviktors@gmail.com>
2023-03-28 13:26:18 +0300

    Solution v4

--- services/order.py
+++ services/order.py
@@ -6,34 +6,34 @@ from db.models import Order, Ticket, MovieSession
 from django.contrib.auth import get_user_model
 
 
+@transaction.atomic()
 def create_order(
         tickets: list,
         username: str,
         date: Optional[str] = None,
 ) -> None:
-    with transaction.atomic():
-        order = Order.objects.create(
-            user=get_user_model().objects.get(username=username)
-        )
-        if date:
-            order.created_at = date
-        order.save()
+    order = Order.objects.create(
+        user=get_user_model().objects.get(username=username)
+    )
+    if date:
+        order.created_at = date
+    order.save()
 
-        Ticket.objects.bulk_create(
-            [
-                Ticket(
-                    movie_session=MovieSession.objects.get(
-                        id=ticket["movie_session"]
-                    ),
-                    order=order,
-                    row=ticket["row"],
-                    seat=ticket["seat"],
-                ) for ticket in tickets
-            ]
-        )
+    Ticket.objects.bulk_create(
+        [
+            Ticket(
+                movie_session=MovieSession.objects.get(
+                    id=ticket["movie_session"]
+                ),
+                order=order,
+                row=ticket["row"],
+                seat=ticket["seat"],
+            ) for ticket in tickets
+        ]
+    )
 
 
-def get_orders(username: str = None) -> QuerySet:
+def get_orders(username: Optional[str] = None) -> QuerySet:
     if username:
         return Order.objects.filter(user__username=username)
     return Order.objects.all()
