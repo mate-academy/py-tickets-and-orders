96d8387d0a3443634a35dbee8b065a89fd5aa28f
Tetiana Kozakova <kleotan901@gmail.com>
2023-04-21 20:14:46 +0200

    solution

--- settings.py
+++ settings.py
@@ -34,18 +34,22 @@ AUTH_USER_MODEL = "db.User"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth."
+                "password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        'OPTIONS': {
-            'min_length': 9,
+        "NAME": "django.contrib.auth."
+                "password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 9,
         }
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth."
+                "password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth."
+                "password_validation.NumericPasswordValidator",
     },
 ]
