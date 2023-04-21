ad6d699aea58900f87f85ae4b06b9f13b7b789c5
Roman28101 <r.kovalenko28101@gmail.com>
2023-04-21 18:51:01 +0800

    Solution fixed

--- services/movie_session.py
+++ services/movie_session.py
@@ -45,7 +45,7 @@ def delete_movie_session_by_id(session_id: int) -> None:
 
 
 def get_taken_seats(movie_session_id: int) -> list[dict]:
-    list_seats = []
-    for seat in Ticket.objects.filter(movie_session_id=movie_session_id):
-        list_seats.append({"row": seat.row, "seat": seat.seat})
-    return list_seats
+    return [
+        {"row": seat.row, "seat": seat.seat}
+        for seat in Ticket.objects.filter(movie_session_id=movie_session_id)
+    ]
--- services/user.py
+++ services/user.py
@@ -2,6 +2,27 @@ from typing import Optional
 from django.contrib.auth import get_user_model
 from django.db.models import QuerySet
 
+from db.models import User
+
+
+def set_data(user: User,
+             username: Optional[str],
+             email: Optional[str] = None,
+             first_name: Optional[str] = None,
+             last_name: Optional[str] = None,
+             password: Optional[str] = None,) -> None:
+    if username:
+        user.username = username
+    if email:
+        user.email = email
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
+    if password:
+        user.set_password(password)
+    user.save()
+
 
 def create_user(username: str,
                 password: str,
@@ -11,13 +32,11 @@ def create_user(username: str,
                 ) -> None:
     user = get_user_model().objects.create_user(
         username=username, password=password)
-    if email:
-        user.email = email
-    if first_name:
-        user.first_name = first_name
-    if last_name:
-        user.last_name = last_name
-    user.save()
+    set_data(user=user,
+             username=username,
+             email=email,
+             first_name=first_name,
+             last_name=last_name)
 
 
 def get_user(user_id: int) -> QuerySet:
@@ -32,14 +51,9 @@ def update_user(user_id: int,
                 last_name: Optional[str] = None
                 ) -> None:
     user = get_user_model().objects.get(id=user_id)
-    if username:
-        user.username = username
-    if email:
-        user.email = email
-    if first_name:
-        user.first_name = first_name
-    if last_name:
-        user.last_name = last_name
-    if password:
-        user.set_password(password)
-    user.save()
+    set_data(user=user,
+             username=username,
+             password=password,
+             email=email,
+             first_name=first_name,
+             last_name=last_name)
