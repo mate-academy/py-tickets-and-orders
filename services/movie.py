56de1190959c7ea781180413e16fda94f36b93ff
Olena Liubynetska <olena.liuby@gmail.com>
2023-04-30 22:12:20 +0100

    Solution

--- services/movie.py
+++ services/movie.py
@@ -1,19 +1,27 @@
+from typing import Optional
+
+from django.db import transaction
 from django.db.models import QuerySet
 
 from db.models import Movie
 
 
 def get_movies(
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None,
+        genres_ids: Optional[list[int]] = None,
+        actors_ids: Optional[list[int]] = None,
+        title: Optional[str] = None
 ) -> QuerySet:
+
     queryset = Movie.objects.all()
 
-    if genres_ids:
+    if genres_ids is not None:
         queryset = queryset.filter(genres__id__in=genres_ids)
 
-    if actors_ids:
+    if actors_ids is not None:
         queryset = queryset.filter(actors__id__in=actors_ids)
+
+    if title is not None:
+        queryset = queryset.filter(title__icontains=title)
 
     return queryset
 
@@ -23,18 +31,20 @@ def get_movie_by_id(movie_id: int) -> Movie:
 
 
 def create_movie(
-    movie_title: str,
-    movie_description: str,
-    genres_ids: list = None,
-    actors_ids: list = None,
+        movie_title: str,
+        movie_description: str,
+        genres_ids: Optional[list[int]] = None,
+        actors_ids: Optional[list[int]] = None,
 ) -> Movie:
-    movie = Movie.objects.create(
-        title=movie_title,
-        description=movie_description,
-    )
-    if genres_ids:
-        movie.genres.set(genres_ids)
-    if actors_ids:
-        movie.actors.set(actors_ids)
+    with transaction.atomic():
+        movie = Movie.objects.create(
+            title=movie_title,
+            description=movie_description,
+        )
 
-    return movie
+        if genres_ids is not None:
+            movie.genres.set(genres_ids)
+        if actors_ids is not None:
+            movie.actors.set(actors_ids)
+
+        return movie
