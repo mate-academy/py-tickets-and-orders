270367a8dee1be6d63d592ec11a00502148204f7
Nochvai Maksym <123680608+MaksNochvai@users.noreply.github.com>
2023-05-04 13:52:30 +0300

    Solution++

--- services/movie.py
+++ services/movie.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from django.db import transaction
 from django.db.models import QuerySet
 
@@ -5,9 +7,9 @@ from db.models import Movie
 
 
 def get_movies(
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None,
-    title: str = None
+    genres_ids: Optional[list[int]] = None,
+    actors_ids: Optional[list[int]] = None,
+    title: Optional[str] = None
 ) -> QuerySet:
     queryset = Movie.objects.all()
     if genres_ids:
@@ -23,20 +25,20 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
+@transaction.atomic()
 def create_movie(
     movie_title: str,
     movie_description: str,
     genres_ids: list = None,
     actors_ids: list = None,
 ) -> Movie:
-    with transaction.atomic():
-        movie = Movie.objects.create(
-            title=movie_title,
-            description=movie_description,
-        )
-        if genres_ids:
-            movie.genres.set(genres_ids)
-        if actors_ids:
-            movie.actors.set(actors_ids)
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
 
-        return movie
+    return movie
--- services/order.py
+++ services/order.py
@@ -6,24 +6,24 @@ from django.db import transaction
 from db.models import Order, Ticket
 
 
+@transaction.atomic()
 def create_order(tickets: list[dict],
                  username: str,
                  date: Optional[str] = None) -> Order:
-    with transaction.atomic():
-        new_order = Order.objects.create(
-            user=get_user_model().objects.get(username=username)
+    new_order = Order.objects.create(
+        user=get_user_model().objects.get(username=username)
+    )
+    if date:
+        new_order.created_at = date
+    for ticket_data in tickets:
+        Ticket.objects.create(
+            order=new_order,
+            movie_session_id=ticket_data["movie_session"],
+            row=ticket_data["row"],
+            seat=ticket_data["seat"]
         )
-        if date:
-            new_order.created_at = date
-        for ticket_data in tickets:
-            Ticket.objects.create(
-                order=new_order,
-                movie_session_id=ticket_data["movie_session"],
-                row=ticket_data["row"],
-                seat=ticket_data["seat"]
-            )
-        new_order.save()
-        return new_order
+    new_order.save()
+    return new_order
 
 
 def get_orders(username: Optional[str] = None) -> Order:
