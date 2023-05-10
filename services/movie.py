5fc0a0733813143e5d52435e46df240351800e89
Anatoliy Pilipey <pilipeytolya@gmail.com>
2023-05-10 21:52:13 +0300

    Solution

--- services/movie.py
+++ services/movie.py
@@ -1,25 +1,26 @@
+from django.db import transaction
 from django.db.models import QuerySet
 
 from db.models import Movie
 
 
 def get_movies(
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None,
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None,
+        title: str = None
 ) -> QuerySet:
     queryset = Movie.objects.all()
-
-    if genres_ids:
-        queryset = queryset.filter(genres__id__in=genres_ids)
-
-    if actors_ids:
-        queryset = queryset.filter(actors__id__in=actors_ids)
-
+    if title:
+        if genres_ids:
+            queryset = queryset.filter(genres__id__in=genres_ids)
+        if actors_ids:
+            queryset = queryset.filter(actors__id__in=actors_ids)
+        queryset = queryset.filter(title__icontains=title).values_list("title")
     return queryset
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
-    return Movie.objects.get(id=movie_id)
+    return Movie.objects.get(pk=movie_id)
 
 
 def create_movie(
@@ -28,13 +29,13 @@ def create_movie(
     genres_ids: list = None,
     actors_ids: list = None,
 ) -> Movie:
-    movie = Movie.objects.create(
-        title=movie_title,
-        description=movie_description,
-    )
-    if genres_ids:
-        movie.genres.set(genres_ids)
-    if actors_ids:
-        movie.actors.set(actors_ids)
-
-    return movie
+    with transaction.atomic():
+        movie = Movie.objects.create(
+            title=movie_title,
+            description=movie_description,
+        )
+        if genres_ids:
+            movie.genres.set(genres_ids)
+        if actors_ids:
+            movie.actors.set(actors_ids)
+        return movie
---
+++ services/order.py
@@ -0,0 +1,34 @@
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Order, Ticket, MovieSession
+
+
+def create_order(tickets: list, username: str, date: str = None) -> Order:
+    user = get_user_model().objects.get(username=username)
+    with transaction.atomic():
+        order = Order.objects.create(user=user)
+        if date:
+            order.created_at = date
+            order.save()
+
+        for ticket in tickets:
+            Ticket.objects.create(
+                movie_session=MovieSession.objects.get(
+                    pk=ticket["movie_session"]
+                ),
+                order=order,
+                row=ticket["row"],
+                seat=ticket["seat"]
+            )
+    return order
+
+
+def get_orders(username: str = None) -> QuerySet:
+    order = Order.objects.all()
+    if username is not None:
+        order = order.filter(
+            user=get_user_model().objects.get(username=username)
+        )
+    return order
