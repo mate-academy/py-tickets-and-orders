a2f52883ebc267e1e82bdbf8179918ae7289ed33
Ivan Hutsuliak <ivan.guculyak@gmail.com>
2023-05-12 22:01:48 +0300

    'solution'

--- services/user.py
+++ services/user.py
@@ -13,12 +13,16 @@ def create_user(
         username=username,
         password=password,
     )
+
     if email:
         new_user.email = email
+
     if first_name:
         new_user.first_name = first_name
+
     if last_name:
         new_user.last_name = last_name
+
     new_user.save()
 
 
@@ -35,14 +39,20 @@ def update_user(
     last_name: str = None,
 ) -> None:
     up_user = get_user_model().objects.get(id=user_id)
+
     if username:
         up_user.username = username
+
     if password:
         up_user.set_password(password)
+
     if email:
         up_user.email = email
+
     if first_name:
         up_user.first_name = first_name
+
     if last_name:
         up_user.last_name = last_name
+
     up_user.save()
