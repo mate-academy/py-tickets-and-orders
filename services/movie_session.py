0c55bd33003b9b098e79f5375a06faa20f911855
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-06-11 16:35:48 +0300

    Implement the tickets and orders task

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,10 +1,14 @@
+from typing import Optional
+
 from django.db.models import QuerySet
 
 from db.models import MovieSession
 
 
 def create_movie_session(
-    movie_show_time: str, movie_id: int, cinema_hall_id: int
+    movie_show_time: str,
+    movie_id: int,
+    cinema_hall_id: int
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
@@ -13,10 +17,14 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet:
+def get_movies_sessions(
+    session_date: Optional[str] = None
+) -> QuerySet:
     queryset = MovieSession.objects.all()
+
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
+
     return queryset
 
 
@@ -26,19 +34,34 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 def update_movie_session(
     session_id: int,
-    show_time: str = None,
-    movie_id: int = None,
-    cinema_hall_id: int = None,
+    show_time: Optional[str] = None,
+    movie_id: Optional[int] = None,
+    cinema_hall_id: Optional[int] = None,
 ) -> None:
     movie_session = MovieSession.objects.get(id=session_id)
+
     if show_time:
         movie_session.show_time = show_time
+
     if movie_id:
         movie_session.movie_id = movie_id
+
     if cinema_hall_id:
         movie_session.cinema_hall_id = cinema_hall_id
+
     movie_session.save()
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    session = MovieSession.objects.filter(id=movie_session_id).values(
+        "ticket__row", "ticket__seat"
+    )
+
+    return [
+        {"row": ticket["ticket__row"], "seat": ticket["ticket__seat"]}
+        for ticket in session
+    ]
