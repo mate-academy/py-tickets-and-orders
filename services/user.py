6482e3a51c982047c8cff393f1a32fb2a55c5281
KirillMelanich <kirillmelanich@gmail.com>
2023-06-14 22:14:04 +0300

    'Solution'

--- services/user.py
+++ services/user.py
@@ -1,7 +1,5 @@
 from db.models import User
 
-from typing import Optional
-
 
 def create_user(*args, **kwargs) -> User:
     user = User.objects.create_user(
@@ -15,29 +13,15 @@ def get_user(user_id: int) -> User:
     return User.objects.get(id=user_id)
 
 
-def update_user(user_id: int,
-                username: Optional[str] = None,
-                password: Optional[str] = None,
-                email: Optional[str] = None,
-                first_name: Optional[str] = None,
-                last_name: Optional[str] = None) -> User:
-
+def update_user(user_id: int, **kwargs) -> User:
     user = User.objects.get(id=user_id)
 
-    if username is not None:
-        user.username = username
-
-    if password is not None:
-        user.set_password(password)
-
-    if email is not None:
-        user.email = email
-
-    if first_name is not None:
-        user.first_name = first_name
-
-    if last_name is not None:
-        user.last_name = last_name
+    for key, value in kwargs.items():
+        if value is not None:
+            if key == "password":
+                user.set_password(value)
+            else:
+                setattr(user, key, value)
 
     user.save()
     return user
