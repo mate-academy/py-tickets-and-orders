83e06c763156a72c60f0b4e7901c805331bd89e8
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-06-19 22:38:54 +0300

    fix py-tickets-and-orders

--- services/user.py
+++ services/user.py
@@ -8,21 +8,21 @@ def create_user(
         password: str,
         email: Optional[str] = None,
         first_name: Optional[str] = None,
-        last_name: Optional[str] = None
+        last_name: Optional[str] = None,
 ) -> None:
     new_user = User.objects.create_user(
         username=username,
         password=password
     )
+    attributes = {
+        "email": email,
+        "first_name": first_name,
+        "last_name": last_name
+    }
 
-    if email:
-        new_user.email = email
-
-    if first_name:
-        new_user.first_name = first_name
-
-    if last_name:
-        new_user.last_name = last_name
+    for attribute, value in attributes.items():
+        if value:
+            setattr(new_user, attribute, value)
 
     new_user.save()
 
@@ -37,7 +37,7 @@ def update_user(
         password: Optional[str] = None,
         email: Optional[str] = None,
         first_name: Optional[str] = None,
-        last_name: Optional[str] = None
+        last_name: Optional[str] = None,
 ) -> None:
     user_to_update = get_user(user_id)
 
