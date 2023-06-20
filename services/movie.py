1470501f99037f887ce93aa9680ebc1c41a21d75
Oleksandr Gubarev <alexgubarev885@gmail.com>
2023-06-20 13:58:28 +0300

    add optional type py-tickets-and-orders

--- services/movie.py
+++ services/movie.py
@@ -7,8 +7,8 @@ from db.models import Movie
 
 
 def get_movies(
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None,
+        genres_ids: Optional[list[int]] = None,
+        actors_ids: Optional[list[int]] = None,
         title: Optional[str] = None
 ) -> QuerySet:
     queryset = Movie.objects.all()
@@ -18,7 +18,7 @@ def get_movies(
     if actors_ids:
         queryset = queryset.filter(actors__id__in=actors_ids)
 
-    if title:
+    if title is not None:
         queryset = queryset.filter(title__icontains=title)
 
     return queryset
@@ -32,8 +32,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 def create_movie(
         movie_title: str,
         movie_description: str,
-        genres_ids: list = None,
-        actors_ids: list = None,
+        genres_ids: Optional[list[int]] = None,
+        actors_ids: Optional[list[int]] = None,
 ) -> Movie:
     movie = Movie.objects.create(
         title=movie_title,
