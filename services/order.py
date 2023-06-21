75c5a7e56604fc330aa5f49eb280511cdf0e5a43
Bohdan11Dii <bodyabrovdiy@gmail.com>
2023-06-21 13:07:32 +0300

    use @transaction.atomic() decorator

--- services/order.py
+++ services/order.py
@@ -5,33 +5,34 @@ from django.db.models import QuerySet
 from typing import Optional
 
 
+@transaction.atomic()
 def create_order(
         tickets: list[dict],
         username: str,
         date: Optional[str] = None
 ) -> Order:
-    with transaction.atomic():
-        order = Order.objects.create(
-            user=get_user_model().objects.get(username=username)
-        )
 
-        if date is not None:
-            Order.objects.filter(id=order.id).update(created_at=date)
+    order = Order.objects.create(
+        user=get_user_model().objects.get(username=username)
+    )
 
-        Ticket.objects.bulk_create(
-            [
-                Ticket(
-                    row=ticket["row"],
-                    seat=ticket["seat"],
-                    movie_session=MovieSession.objects.get(
-                        id=ticket["movie_session"]
-                    ),
-                    order=order
-                )
-                for ticket in tickets
-            ]
-        )
-        return order
+    if date is not None:
+        Order.objects.filter(id=order.id).update(created_at=date)
+
+    Ticket.objects.bulk_create(
+        [
+            Ticket(
+                row=ticket["row"],
+                seat=ticket["seat"],
+                movie_session=MovieSession.objects.get(
+                    id=ticket["movie_session"]
+                ),
+                order=order
+            )
+            for ticket in tickets
+        ]
+    )
+    return order
 
 
 def get_orders(username: Optional[str] = None) -> QuerySet:
