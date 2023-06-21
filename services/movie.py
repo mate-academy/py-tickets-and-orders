679771084eef7f54a693460138a6c1ac162b42a5
Yaroslav Rybalka <markus9869@gmail.com>
2023-06-21 13:42:31 +0300

    Solution

--- services/movie.py
+++ services/movie.py
@@ -5,8 +5,8 @@ from db.models import Movie
 
 def get_movies(
     genres_ids: list[int] = None,
-        actors_ids: list[int] = None,
-        title: str = None
+    actors_ids: list[int] = None,
+    title: str = None
 ) -> QuerySet:
     queryset = Movie.objects.all()
 
--- services/user.py
+++ services/user.py
@@ -1,22 +1,12 @@
-from sqlite3 import IntegrityError
-
-from django.core.exceptions import ValidationError
-
 from db.models import User
 
 
-def create_user(
-    username: str,
-    password: str,
-    email: str = None,
-    first_name: str = None,
-    last_name: str = None,
+def create_user_data(
+        user: User,
+        email: str = None,
+        first_name: str = None,
+        last_name: str = None,
 ) -> None:
-    try:
-        user = User.objects.create_user(username=username, password=password)
-    except IntegrityError:
-        raise ValidationError("Username already exists")
-
     if email:
         user.email = email
     if first_name:
@@ -25,6 +15,18 @@ def create_user(
         user.last_name = last_name
 
     user.save()
+
+
+def create_user(
+        username: str,
+        password: str,
+        email: str = None,
+        first_name: str = None,
+        last_name: str = None,
+) -> None:
+    user = User.objects.create_user(username=username, password=password)
+
+    create_user_data(user, email, first_name, last_name)
 
 
 def get_user(user_id: int) -> User:
@@ -33,23 +35,16 @@ def get_user(user_id: int) -> User:
 
 
 def update_user(
-    user_id: int,
-    username: str = None,
-    password: str = None,
-    email: str = None,
-    first_name: str = None,
-    last_name: str = None,
+        user_id: int,
+        username: str = None,
+        password: str = None,
+        email: str = None,
+        first_name: str = None,
+        last_name: str = None,
 ) -> None:
-    user = User.objects.get(id=user_id)
+    user = get_user(user_id)
     if username:
         user.username = username
     if password:
         user.set_password(password)
-    if email:
-        user.email = email
-    if first_name:
-        user.first_name = first_name
-    if last_name:
-        user.last_name = last_name
-
-    user.save()
+    create_user_data(user, email, first_name, last_name)
