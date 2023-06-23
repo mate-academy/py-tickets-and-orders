8b9be394f5db5e692381e7c0d84558c9a77501ed
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-06-23 20:05:29 +0300

    Using decorator @transaction.atomic()

--- services/order.py
+++ services/order.py
@@ -8,31 +8,31 @@ from db.models import Order, Ticket
 users = get_user_model()
 
 
+@transaction.atomic()
 def create_order(
         tickets: list[Ticket],
         username: str,
         date: Optional[str] = None
 ) -> None:
-    with transaction.atomic():
-        user_order = users.objects.get(username=username)
-        new_order = Order.objects.create(user=user_order)
+    user_order = users.objects.get(username=username)
+    new_order = Order.objects.create(user=user_order)
 
-        if date:
-            new_order.created_at = date
-        new_order.save()
+    if date:
+        new_order.created_at = date
+    new_order.save()
 
-        users_tickets = []
-        for ticket in tickets:
-            new_ticket = Ticket(
-                movie_session_id=ticket["movie_session"],
-                order=new_order,
-                row=ticket["row"],
-                seat=ticket["seat"]
-            )
-            new_ticket.clean()
-            users_tickets.append(new_ticket)
+    users_tickets = []
+    for ticket in tickets:
+        new_ticket = Ticket(
+            movie_session_id=ticket["movie_session"],
+            order=new_order,
+            row=ticket["row"],
+            seat=ticket["seat"]
+        )
+        new_ticket.clean()
+        users_tickets.append(new_ticket)
 
-        Ticket.objects.bulk_create(users_tickets)
+    Ticket.objects.bulk_create(users_tickets)
 
 
 def get_orders(username: Optional[str] = None) -> QuerySet:
