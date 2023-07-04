093dba180fc703d017712bbf843fd683c81dec10
Denys Seliverstov <catsperr322@gmail.com>
2023-07-04 19:54:03 +0300

    Fixed

--- services/order.py
+++ services/order.py
@@ -27,7 +27,7 @@ def create_order(tickets: list[dict],
     return order
 
 
-def get_orders(username: str = None) -> Order | QuerySet:
+def get_orders(username: str = None) -> QuerySet[Order]:
     order = Order.objects.all()
     if username:
         order = Order.objects.filter(user__username=username)
