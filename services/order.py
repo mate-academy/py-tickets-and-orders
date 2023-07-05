bd7f21ac6e2af28cc97ce78ed6254b405ef87716
Anton Komarov <anton_komarov_qa@ukr.net>
2023-07-05 16:35:43 +0300

    'Solution'

--- services/order.py
+++ services/order.py
@@ -29,7 +29,7 @@ def create_order(
             )
 
 
-def get_orders(username: str = None) -> QuerySet:
+def get_orders(username: str = None) -> QuerySet[Order]:
     if username:
         return Order.objects.filter(user__username=username)
     return Order.objects.all()
