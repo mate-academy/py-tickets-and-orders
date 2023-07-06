7c393b3564c06b1bbb992390112a136605405223
Eugene Polunin <altahoma@gmail.com>
2023-07-07 01:28:26 +0300

    Implemented Order, Ticket, User models and services for them

---
+++ services/order.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import User, Order, MovieSession, Ticket
+
+
+def create_order(
+    tickets: list[dict], username: str, date: datetime = None
+) -> Order:
+    with transaction.atomic():
+        user = User.objects.get(username=username)
+        order = Order.objects.create(user=user, created_at=date)
+        ticket_instances = []
+
+        if date:
+            order.created_at = date
+        order.save()
+
+        for ticket_data in tickets:
+            row = ticket_data["row"]
+            seat = ticket_data["seat"]
+            movie_session_id = ticket_data["movie_session"]
+            movie_session = MovieSession.objects.get(movie_id=movie_session_id)
+
+            ticket = Ticket(
+                movie_session=movie_session, order=order, row=row, seat=seat
+            )
+            ticket_instances.append(ticket)
+
+        Ticket.objects.bulk_create(ticket_instances)
+
+    return order
+
+
+def get_orders(username: str = None) -> QuerySet:
+    queryset = Order.objects.all()
+    if username:
+        queryset = queryset.filter(user__username=username)
+
+    return queryset
