34934f114799921eba3bf7ac18720dbf8b3f2aea
Oleksii Kolii <igravkompik@gmail.com>
2023-07-06 18:33:50 +0300

    removed context managers added decorators instead

--- services/order.py
+++ services/order.py
@@ -5,26 +5,26 @@ from django.db.models import QuerySet
 from db.models import Order, Ticket
 
 
+@transaction.atomic()
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = None
 ) -> None:
-    with transaction.atomic():
-        user = get_user_model().objects.get(username=username)
+    user = get_user_model().objects.get(username=username)
 
-        order = Order.objects.create(user=user)
-        if date:
-            order.created_at = date
-        order.save()
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = date
+    order.save()
 
-        for ticket in tickets:
-            Ticket.objects.create(
-                row=ticket["row"],
-                seat=ticket["seat"],
-                movie_session_id=ticket["movie_session"],
-                order=order
-            )
+    for ticket in tickets:
+        Ticket.objects.create(
+            row=ticket["row"],
+            seat=ticket["seat"],
+            movie_session_id=ticket["movie_session"],
+            order=order
+        )
 
 
 def get_orders(username: str = None) -> QuerySet:
