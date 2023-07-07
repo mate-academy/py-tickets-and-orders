8736f72fa825254d50569e89089b493170f3d1a3
Denys Seliverstov <catsperr322@gmail.com>
2023-07-07 13:58:50 +0300

    Fixed, probably

--- services/user.py
+++ services/user.py
@@ -5,17 +5,14 @@ from db.models import User
 
 def create_user(username: str,
                 password: str,
-                email: str = None,
-                first_name: str = None,
-                last_name: str = None) -> User:
+                **kwargs: dict[str]) -> User:
     user = get_user_model().objects.create_user(username=username,
                                                 password=password)
-    if email:
-        user.email = email
-    if first_name:
-        user.first_name = first_name
-    if last_name:
-        user.last_name = last_name
+
+    for el in kwargs:
+        if el in ["email", "first_name", "last_name"]:
+            setattr(user, el, kwargs[el])
+
     user.save()
 
     return user
@@ -26,26 +23,13 @@ def get_user(user_id: int) -> User:
 
 
 def update_user(user_id: int,
-                username: str = None,
-                password: str = None,
-                email: str = None,
-                first_name: str = None,
-                last_name: str = None) -> None:
+                **kwargs: dict[str]) -> None:
     user = get_user_model().objects.get(id=user_id)
 
-    if username:
-        user.username = username
-
-    if password:
-        user.set_password(password)
-
-    if email:
-        user.email = email
-
-    if first_name:
-        user.first_name = first_name
-
-    if last_name:
-        user.last_name = last_name
+    for el in kwargs:
+        if el in ["username", "email", "first_name", "last_name"]:
+            setattr(user, el, kwargs[el])
+        if el == "password":
+            user.set_password(kwargs[el])
 
     user.save()
