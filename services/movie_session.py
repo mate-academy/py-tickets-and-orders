cf8dcec28beb09a85d5bd3365f814b542b5e2a0f
YOUR_NAME <YOUR_EMAIL>
2023-07-09 14:34:29 +0300

    Tickets and orders: use only return in "get taken seats"

--- services/movie_session.py
+++ services/movie_session.py
@@ -49,8 +49,6 @@ def delete_movie_session_by_id(session_id: int) -> None:
 
 
 def get_taken_seats(movie_session_id: int) -> List[Dict[str, int]]:
-    taken_seats = Ticket.objects.filter(
+    return list(Ticket.objects.filter(
         movie_session_id=movie_session_id
-    ).values("row", "seat")
-
-    return list(taken_seats)
+    ).values("row", "seat"))
