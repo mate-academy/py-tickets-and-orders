6745978bd13e80de06befa91983be9f5a74d2cea
Olha Tryhub <olha.tryhub@nure.ua>
2023-07-11 00:22:15 +0300

    Solution for Tickets and Orders

--- db/models.py
+++ db/models.py
@@ -1,4 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.db.models import UniqueConstraint
 from django.db import models
+
+import settings
 
 
 class Genre(models.Model):
@@ -17,7 +22,7 @@ class Actor(models.Model):
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, db_index=True)
     description = models.TextField()
     actors = models.ManyToManyField(to=Actor)
     genres = models.ManyToManyField(to=Genre)
@@ -46,3 +51,56 @@ class MovieSession(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.created_at}"
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey(to=MovieSession,
+                                      on_delete=models.CASCADE)
+    order = models.ForeignKey(to=Order, on_delete=models.CASCADE)
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        title = self.movie_session.movie.title
+        show_time = self.movie_session.show_time
+        return f"{title} {show_time} (row: {self.row}, seat: {self.seat})"
+
+    def clean(self) -> None:
+        rows = self.movie_session.cinema_hall.rows
+        seats = self.movie_session.cinema_hall.seats_in_row
+        errors_list = [
+            {"row": [f"row number must be in available range: "
+                     f"(1, rows): (1, {rows})"]},
+            {"seat": [f"seat number must be in available range: "
+                      f"(1, seats_in_row): (1, {seats})"]}
+        ]
+        if self.row > rows:
+            raise ValidationError(errors_list[0])
+        if self.seat > seats:
+            raise ValidationError(errors_list[1])
+
+    def save(self, *args, **kwargs) -> None:
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=["row", "seat", "movie_session"],
+                             name="unique_ticket")
+        ]
+
+
+class User(AbstractUser):
+    pass
