e1aac25f77205473180ac26288594e3d94e4ae92
Eugene Polunin <altahoma@gmail.com>
2023-07-10 14:29:51 +0300

    Hot fix get_taken_seats()

--- services/movie_session.py
+++ services/movie_session.py
@@ -45,6 +45,8 @@ def delete_movie_session_by_id(session_id: int) -> None:
 
 
 def get_taken_seats(movie_session_id: int) -> list[dict]:
-    tickets = get_movie_session_by_id(movie_session_id).ticket_set.all()
-
-    return [{"row": row, "seat": seat} for row, seat in tickets.values()]
+    return list(
+        get_movie_session_by_id(movie_session_id).ticket_set.values(
+            "row", "seat"
+        )
+    )
