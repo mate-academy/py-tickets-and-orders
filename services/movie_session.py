5d8bfeef3596db8b33eafaea6ec6cb2a09eb7d74
Alex Yatsun <starcrafter4444@gmail.com>
2023-07-11 14:49:22 +0300

    solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -45,7 +45,6 @@ def delete_movie_session_by_id(session_id: int) -> None:
 
 
 def get_taken_seats(movie_session_id: id) -> list[dict]:
-    return [
-        {"row": ticket.row, "seat": ticket.seat} for ticket
-        in Ticket.objects.filter(movie_session__id=movie_session_id)
-    ]
+    return list(Ticket.objects.filter(
+                movie_session__id=movie_session_id
+                ).values("row", "seat"))
