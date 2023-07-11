f5e454bd351b12b46f2fdcd5fdfbcf3ed3dd5494
Rostyslav <rostyslav.furman@gmail.com>
2023-07-11 17:29:01 +0300

    my_commit

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,12 +1,12 @@
-from django.db.models import QuerySet
+from django.db.models import QuerySet, F
 
 from db.models import MovieSession
 
 
 def create_movie_session(
     movie_show_time: str, movie_id: int, cinema_hall_id: int
-) -> MovieSession:
-    return MovieSession.objects.create(
+) -> None:
+    MovieSession.objects.create(
         show_time=movie_show_time,
         movie_id=movie_id,
         cinema_hall_id=cinema_hall_id,
@@ -42,3 +42,11 @@ def update_movie_session(
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    return list(MovieSession.objects.filter(
+        id=movie_session_id).values(
+            row=F("tickets__row"),
+            seat=F("tickets__seat")
+    ))
