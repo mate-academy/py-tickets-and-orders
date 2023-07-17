66b3ae0622cd283bded6e96ed15c7d10def1b6db
Dukh Vlad <84961728+RedCatHot@users.noreply.github.com>
2023-07-17 18:50:18 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -60,7 +60,8 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE)
+        on_delete=models.CASCADE,
+    )
 
     class Meta:
         ordering = ["-created_at"]
