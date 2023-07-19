c9f0babc0207df515f9d345fb85407d85c8489b3
Vlad <vladislav.zhyla@gmail.com>
2023-07-19 09:54:41 +0300

    Create and update user funcs upgraded

--- services/user.py
+++ services/user.py
@@ -6,24 +6,13 @@ from db.models import User
 def create_user(
         username: str,
         password: str,
-        email: str = None,
-        first_name: str = "",
-        last_name: str = ""
+        **kwargs
 ) -> None:
     new_user = get_user_model().objects.create_user(
         username=username,
         password=password,
-        email=email,
-        first_name=first_name,
-        last_name=last_name
+        **kwargs
     )
-    if email:
-        new_user.email = email
-    if first_name:
-        new_user.first_name = first_name
-    if last_name:
-        new_user.last_name = last_name
-
     new_user.save()
 
 
@@ -31,24 +20,13 @@ def get_user(user_id: int) -> User:
     return get_user_model().objects.get(id=user_id)
 
 
-def update_user(
-        user_id: int,
-        username: str = None,
-        password: str = None,
-        email: str = None,
-        first_name: str = None,
-        last_name: str = None
-) -> None:
-    updated_user = get_user(user_id)
-    if username:
-        updated_user.username = username
-    if password:
-        updated_user.set_password(password)
-    if email:
-        updated_user.email = email
-    if first_name:
-        updated_user.first_name = first_name
-    if last_name:
-        updated_user.last_name = last_name
+def update_user(user_id: int, **kwargs) -> None:
+    updated_user = get_user_model().objects.get(id=user_id)
+
+    for field, value in kwargs.items():
+        if field == "password":
+            updated_user.set_password(value)
+        else:
+            setattr(updated_user, field, value)
 
     updated_user.save()
