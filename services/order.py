c57d1b828816205283277bdac8e80ef3f4144438
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-07-21 19:52:29 +0300

    add setattr add decorator transaction

--- services/order.py
+++ services/order.py
@@ -4,20 +4,20 @@ from db.models import Order, Ticket
 from django.db import transaction
 
 
+@transaction.atomic()
 def create_order(tickets: list[dict], username: str, date: str = None) -> None:
-    with transaction.atomic():
-        user = get_user_model().objects.get(username=username)
-        order = Order.objects.create(user=user)
-        if date:
-            order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
-            order.save()
-        for ticket in tickets:
-            Ticket.objects.create(
-                movie_session_id=ticket["movie_session"],
-                row=ticket["row"],
-                seat=ticket["seat"],
-                order=order,
-            )
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+        order.save()
+    for ticket in tickets:
+        Ticket.objects.create(
+            movie_session_id=ticket["movie_session"],
+            row=ticket["row"],
+            seat=ticket["seat"],
+            order=order,
+        )
 
 
 def get_orders(username: str = None) -> Order:
