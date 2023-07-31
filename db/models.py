9d1fd603df13eb8be65d96d28946ab3f76e2c297
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2023-07-31 21:03:20 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,24 +1,29 @@
+from datetime import datetime
+from typing import Any
+
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name: str = models.CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
         return self.name
 
 
 class Actor(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
+    first_name: str = models.CharField(max_length=255)
+    last_name: str = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
+    title: str = models.CharField(max_length=255, db_index=True)
+    description: str = models.TextField()
     actors = models.ManyToManyField(to=Actor)
     genres = models.ManyToManyField(to=Genre)
 
@@ -27,9 +32,9 @@ class Movie(models.Model):
 
 
 class CinemaHall(models.Model):
-    name = models.CharField(max_length=255)
-    rows = models.IntegerField()
-    seats_in_row = models.IntegerField()
+    name: str = models.CharField(max_length=255)
+    rows: int = models.IntegerField()
+    seats_in_row: int = models.IntegerField()
 
     @property
     def capacity(self) -> int:
@@ -40,9 +45,80 @@ class CinemaHall(models.Model):
 
 
 class MovieSession(models.Model):
-    show_time = models.DateTimeField()
-    cinema_hall = models.ForeignKey(to=CinemaHall, on_delete=models.CASCADE)
-    movie = models.ForeignKey(to=Movie, on_delete=models.CASCADE)
+    show_time: datetime = models.DateTimeField()
+    cinema_hall: CinemaHall = models.ForeignKey(
+        to=CinemaHall,
+        on_delete=models.CASCADE
+    )
+    movie: Movie = models.ForeignKey(to=Movie, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class User(AbstractUser):
+    pass
+
+
+class Order(models.Model):
+    created_at: datetime = models.DateTimeField(auto_now_add=True)
+    user: User = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return self.created_at.strftime("%Y-%m-%d %H:%M:%S")
+
+
+class Ticket(models.Model):
+    movie_session: MovieSession = models.ForeignKey(
+        MovieSession,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    order: Order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    row: int = models.IntegerField()
+    seat: int = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["row", "seat", "movie_session"],
+                name="unique_active"
+            )
+        ]
+
+    def save(
+            self,
+            force_insert: bool = False,
+            force_update: bool = False,
+            using: Any = None,
+            update_fields: Any = None
+    ) -> None:
+        self.full_clean()
+        super(Ticket, self).save(
+            force_insert,
+            force_update,
+            using,
+            update_fields
+        )
+
+    def clean(self) -> None:
+        if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
+            raise ValidationError({
+                "row": f"row number must be in available range: "
+                       f"(1, rows): "
+                       f"(1, {self.row - 1})"
+            })
+        if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
+            raise ValidationError({
+                "seat": f"seat number must be in available range: "
+                        f"(1, seats_in_row): "
+                        f"(1, {self.seat - 1})"
+            })
+
+    def __str__(self) -> str:
+        title = self.movie_session.movie.title
+        time = self.movie_session.show_time.strftime("%Y-%m-%d %H:%M:%S")
+        return f"{title} {time} (row: {self.row}, seat: {self.seat})"
---
+++ services/order.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Order, User, Ticket
+
+
+@transaction.atomic
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: str = None
+) -> Order:
+    user = User.objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+    order.save()
+    for data in tickets:
+        Ticket.objects.create(
+            row=data["row"],
+            seat=data["seat"],
+            movie_session_id=data["movie_session"],
+            order=order
+        )
+    return order
+
+
+def get_orders(username: str = None) -> QuerySet[Order]:
+    if username:
+        return Order.objects.filter(user__username=username)
+    return Order.objects.all()
