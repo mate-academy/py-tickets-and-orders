dcde77baecd9e531125d15f3b2ab665c41b6d7f8
Oleksandr <@gmail.com>
2023-08-16 08:39:45 +0300

    'Fixed2'

--- services/movie_session.py
+++ services/movie_session.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from django.db.models import QuerySet
 
-from db.models import MovieSession
+from db.models import MovieSession, Ticket
 
 
 def create_movie_session(
@@ -57,11 +57,11 @@ def delete_movie_session_by_id(session_id: int) -> None:
 
 
 def get_taken_seats(movie_session_id: int) -> list[dict]:
-    session = MovieSession.objects.filter(id=movie_session_id).values(
-        "ticket__row", "ticket__seat"
+    session = Ticket.objects.filter(movie_session_id=movie_session_id).values(
+        "row", "seat"
     )
 
     return [
-        {"row": ticket["ticket__row"], "seat": ticket["ticket__seat"]}
+        {"row": ticket["row"], "seat": ticket["seat"]}
         for ticket in session
     ]
--- services/user.py
+++ services/user.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from django.contrib.auth import get_user_model
 
 from db.models import User
@@ -8,23 +6,16 @@ from db.models import User
 def create_user(
     username: str,
     password: str,
-    email: Optional[str] = None,
-    first_name: Optional[str] = None,
-    last_name: Optional[str] = None,
+    **kwargs
 ) -> User:
     user = get_user_model().objects.create_user(
         username=username,
         password=password,
     )
 
-    if email:
-        setattr(user, "email", email)
-
-    if first_name:
-        setattr(user, "first_name", first_name)
-
-    if last_name:
-        setattr(user, "last_name", last_name)
+    for attr_name, attr_value in kwargs.items():
+        if attr_value:
+            setattr(user, attr_name, attr_value)
 
     user.save()
 
@@ -37,27 +28,14 @@ def get_user(user_id: int) -> User:
 
 def update_user(
     user_id: int,
-    username: Optional[str] = None,
-    password: Optional[str] = None,
-    email: Optional[str] = None,
-    first_name: Optional[str] = None,
-    last_name: Optional[str] = None,
+    **kwargs,
 ) -> None:
     user = get_user_model().objects.get(id=user_id)
 
-    if username:
-        user.username = username
-
-    if password:
-        user.set_password(password)
-
-    if email:
-        user.email = email
-
-    if first_name:
-        user.first_name = first_name
-
-    if last_name:
-        user.last_name = last_name
+    for attr_name, attr_value in kwargs.items():
+        if attr_name == "password":
+            user.set_password(attr_value)
+        else:
+            setattr(user, attr_name, attr_value)
 
     user.save()
