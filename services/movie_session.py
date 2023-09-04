72ec35ac0029d632b4a49e18f2142e10cd425161
Vasya Turok <turokvasil0@gmail.com>
2023-09-04 21:05:49 +0300

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,5 +1,5 @@
 from django.db.models import QuerySet
-
+import init_django_orm  # noqa : F401
 from db.models import MovieSession
 
 
@@ -42,3 +42,18 @@ def update_movie_session(
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    movie_session = MovieSession.objects.get(
+        id=movie_session_id
+    )
+    result = []
+    for ticket in movie_session.tickets.all():
+        result.append(
+            {
+                "row": ticket.row,
+                "seat": ticket.seat
+            }
+        )
+    return result
