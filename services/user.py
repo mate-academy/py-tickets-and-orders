e019431c959ab59f2f40e389f474e2767dba9e64
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2023-09-07 15:22:17 +0300

    Chenges after code review (third)

--- services/user.py
+++ services/user.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+from db.models import User
 
 
 def create_user(
@@ -12,7 +12,7 @@ def create_user(
         first_name: Optional[str] = None,
         last_name: Optional[str] = None
 ) -> None:
-    user = User.objects.create_user(
+    user = get_user_model().objects.create_user(
         username=username,
         password=password
     )
@@ -28,7 +28,7 @@ def create_user(
 
 
 def get_user(user_id: int) -> User:
-    return User.objects.get(pk=user_id)
+    return get_user_model().objects.get(pk=user_id)
 
 
 def update_user(
@@ -39,7 +39,7 @@ def update_user(
         first_name: Optional[str] = None,
         last_name: Optional[str] = None
 ) -> None:
-    user = User.objects.get(pk=user_id)
+    user = get_user_model().objects.get(pk=user_id)
 
     if username is not None:
         user.username = username
