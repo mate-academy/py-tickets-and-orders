0dcf2f0184b6363dcbacfe0f008eb2a51b3d14c6
Olexander Bryl <edlrian814@gmail.com>
2023-09-11 19:18:36 +0300

    solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -42,3 +42,16 @@ def update_movie_session(
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    session = get_movie_session_by_id(movie_session_id)
+    res = []
+    for ticket in session.tickets.all():
+        res.append(
+            {
+                "row": ticket.row,
+                "seat": ticket.seat
+            }
+        )
+    return res
