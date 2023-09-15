e590acbbf14847419b11f7c327b668035958fddb
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-09-15 14:49:20 +0300

    Used Optional for annotations and used @transaction.atomic decorator

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,10 +1,14 @@
+from typing import Optional
+
 from django.db.models import QuerySet
 
 from db.models import MovieSession, Ticket
 
 
 def create_movie_session(
-        movie_show_time: str, movie_id: int, cinema_hall_id: int
+        movie_show_time: str,
+        movie_id: int,
+        cinema_hall_id: int
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
@@ -13,7 +17,9 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet[MovieSession]:
+def get_movies_sessions(
+        session_date: Optional[str] = None
+) -> QuerySet[MovieSession]:
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
@@ -34,9 +40,9 @@ def get_taken_seats(movie_session_id: int) -> list[dict]:
 
 def update_movie_session(
         session_id: int,
-        show_time: str = None,
-        movie_id: int = None,
-        cinema_hall_id: int = None,
+        show_time: Optional[str] = None,
+        movie_id: Optional[int] = None,
+        cinema_hall_id: Optional[int] = None,
 ) -> None:
     movie_session = get_movie_session_by_id(session_id)
     if show_time:
