a782b9f21638d9266d395bf61ab26e3e355142b5
Oleh Chernous <chernous.oleh@gmail.com>
2023-09-18 15:32:08 +0200

    fixed related names to movie_session

--- db/models.py
+++ db/models.py
@@ -51,14 +51,10 @@ class CinemaHall(models.Model):
 class MovieSession(models.Model):
     show_time = models.DateTimeField()
     cinema_hall = models.ForeignKey(
-        to=CinemaHall,
-        on_delete=models.CASCADE,
-        related_name="movie_sessions"
+        to=CinemaHall, on_delete=models.CASCADE, related_name="movie_sessions"
     )
     movie = models.ForeignKey(
-        to=Movie,
-        on_delete=models.CASCADE,
-        related_name="movie_sessions"
+        to=Movie, on_delete=models.CASCADE, related_name="movie_sessions"
     )
 
     def __str__(self) -> str:
