301de45565c61b1b150971387dfca12dd9bf1968
Illia <kononenko2109@gmail.com>
2023-09-25 14:30:11 +0300

    solution fixed errors func create_user corrected

--- services/user.py
+++ services/user.py
@@ -5,12 +5,9 @@ from django.contrib.auth import get_user_model
 from db.models import User
 
 
-def create_user(username: str, password: str, **kwargs) -> None:
-    default_kwargs = {"email": "", "first_name": "", "last_name": ""}
-    kwargs = {**default_kwargs, **kwargs}
-
+def create_user(username: str, password: str, email: str = "", first_name: str = "", last_name: str = "") -> None:
     get_user_model().objects.create_user(
-        username=username, password=password, **kwargs
+        username=username, password=password, email=email, first_name=first_name, last_name=last_name
     )
 
 
