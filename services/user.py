70e93ddaa15a8c916f83a327ebf1d5e043718f60
Mykhailo Korotia <mr.anderson528491@gmail.com>
2023-10-08 16:20:06 +0300

    Solution

--- services/user.py
+++ services/user.py
@@ -6,22 +6,15 @@ from db.models import User
 def create_user(username: str,
                 password: str,
                 email: Optional[str] = None,
-                first_name: Optional[str] = None,
-                last_name: Optional[str] = None) -> None:
+                first_name: Optional[str] = "",
+                last_name: Optional[str] = "") -> None:
     user = get_user_model().objects.create_user(
         username=username,
         password=password,
+        email=email,
+        first_name=first_name,
+        last_name=last_name
     )
-
-    if email:
-        user.email = email
-
-    if first_name:
-        user.first_name = first_name
-
-    if last_name:
-        user.last_name = last_name
-
     user.save()
 
 
