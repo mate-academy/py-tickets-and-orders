437f0024939348604af9a78f213c59d16d8fe459
Alex Pelevin <ya.pelevin1999@gmail.com>
2023-10-15 14:37:37 +0300

    fix

--- services/order.py
+++ services/order.py
@@ -6,25 +6,25 @@ from django.db.models import QuerySet
 from db.models import Ticket, Order, User
 
 
+@transaction.atomic
 def create_order(tickets: list, username: str, date: str = None) -> Order:
     ticket_objects: list[Ticket] = []
-    with transaction.atomic():
-        user = User.objects.get(username=username)
-        order = Order.objects.create(user=user)
-        if date:
-            order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
-            order.save()
+    user = User.objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+        order.save()
 
-        for ticket in tickets:
-            ticket_objects.append(
-                Ticket.objects.create(
-                    row=ticket["row"],
-                    seat=ticket["seat"],
-                    movie_session_id=ticket["movie_session"],
-                    order=order
-                )
+    for ticket in tickets:
+        ticket_objects.append(
+            Ticket.objects.create(
+                row=ticket["row"],
+                seat=ticket["seat"],
+                movie_session_id=ticket["movie_session"],
+                order=order
             )
-        order.tickets.set(ticket_objects)
+        )
+    order.tickets.set(ticket_objects)
 
     return order
 
