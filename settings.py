20dc35a2f7c6f09df82b0211aabbe22b50f51596
asdadaversa <peikqudakeus@gmail.com>
2023-10-26 20:27:03 +0300

    Solution2

--- settings.py
+++ settings.py
@@ -1,6 +1,4 @@
 import os
-import django_extensions
-import extensions as extensions
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
