10d06b9f4b2d1c03d7c150b6c3bbed19748e538f
Marina <marishka28023@gmail.com>
2023-10-30 17:32:38 +0100

    'Solution2'

--- services/user.py
+++ services/user.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from django.contrib.auth import get_user_model
 from db.models import User
 
@@ -5,9 +7,9 @@ from db.models import User
 def create_user(
         username: str,
         password: str,
-        email: str = None,
-        first_name: str = "",
-        last_name: str = "",
+        email: Optional[str] = None,
+        first_name: Optional[str] = None,
+        last_name: Optional[str] = None,
 ) -> User:
 
     user = get_user_model().objects.create_user(
@@ -15,13 +17,13 @@ def create_user(
         password=password
     )
 
-    if email:
+    if email is not None:
         user.email = email
 
-    if first_name:
+    if first_name is not None:
         user.first_name = first_name
 
-    if last_name:
+    if last_name is not None:
         user.last_name = last_name
 
     user.save()
@@ -35,11 +37,11 @@ def get_user(user_id: int) -> User:
 
 def update_user(
         user_id: int,
-        username: str = None,
-        password: str = None,
-        email: str = None,
-        first_name: str = None,
-        last_name: str = None,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        email: Optional[str] = None,
+        first_name: Optional[str] = None,
+        last_name: Optional[str] = None,
 ) -> User:
 
     user = get_user(user_id)
