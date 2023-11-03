c9766b6f64fb09c5d98cc120c687c2204298a816
Daniil Hinzburh <2ilgan1601@gmail.com>
2023-11-03 19:22:59 +0200

    task completed (1)

---
+++ services/user.py
@@ -0,0 +1,48 @@
+from db.models import User
+
+
+def create_user(
+        username: str,
+        password: str,
+        email=None,
+        first_name=None,
+        last_name=None) -> User:
+    user = User.objects.create_user(username, email=email, password=password)
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
+    user.save()
+    return user
+
+
+def get_user(user_id: int) -> User | None:
+    try:
+        return User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return None
+
+
+def update_user(
+        user_id: int,
+        username=None,
+        password=None,
+        email=None,
+        first_name=None,
+        last_name=None) -> User | None:
+    try:
+        user = User.objects.get(id=user_id)
+        if username:
+            user.username = username
+        if password:
+            user.set_password(password)
+        if email:
+            user.email = email
+        if first_name:
+            user.first_name = first_name
+        if last_name:
+            user.last_name = last_name
+        user.save()
+        return user
+    except User.DoesNotExist:
+        return None
