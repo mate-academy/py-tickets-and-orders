0e840d89bebdc7bafffb465ce52726c7d274e7c5
Lytillis <chemurchem@gmail.com>
2023-11-07 19:51:06 +0200

    Changed on_delete for models

--- db/models.py
+++ db/models.py
@@ -72,7 +72,7 @@ class Order(models.Model):
 
 class Ticket(models.Model):
     movie_session = models.ForeignKey(MovieSession, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     row = models.IntegerField()
     seat = models.IntegerField()
 
