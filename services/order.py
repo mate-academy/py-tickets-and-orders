492b571db82db22347582a530f4e2bf4cd77f5ef
naiv <nazarivankiv1@gmail.com>
2023-11-08 16:02:29 +0200

    solution

--- services/order.py
+++ services/order.py
@@ -1,9 +1,10 @@
 from typing import List
 
+from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
 
-from db.models import Order, Ticket, User
+from db.models import Order, Ticket
 
 
 def create_order(
@@ -11,16 +12,14 @@ def create_order(
         username: str,
         date: str = None
 ) -> None:
-    print("CREATE ORDER:\n", "Tiekcets:", tickets, "DATE:", date)
     with transaction.atomic():
         order = Order.objects.create(
-            user_id=User.objects.get(username=username).id
+            user_id=get_user_model().objects.get(username=username).id
         )
 
         if date:
             order.created_at = date
-
-        order.save()
+            order.save()
 
         for ticket in tickets:
             row, seat, movie_session_id = ticket.values()
@@ -36,6 +35,8 @@ def get_orders(username: str = None) -> QuerySet[Order]:
     orders = Order.objects.all()
 
     if username:
-        orders = orders.filter(user_id=User.objects.get(username=username).id)
+        orders = orders.filter(
+            user_id=get_user_model().objects.get(username=username).id
+        )
 
     return orders
