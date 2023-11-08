2a20cb99238ad625560011e81ffd3af95d275e1a
Sergii Pryima <spryima@gmail.com>
2023-11-08 21:56:01 +0200

    @transaction decorator and create() added

--- services/order.py
+++ services/order.py
@@ -5,26 +5,24 @@ from django.db.models import QuerySet
 from db.models import Order, Ticket
 
 
+@transaction.atomic
 def create_order(tickets: list[dict],
                  username: str,
                  date: str = None) -> None:
     user = get_user_model().objects.get(username=username)
 
-    with transaction.atomic():
+    order = Order(user=user)
+    order.save()
 
-        order = Order(user=user)
+    if date:
+        order.created_at = date
         order.save()
 
-        if date:
-            order.created_at = date
-            order.save()
-
-        for ticket in tickets:
-            new_ticket = Ticket(movie_session_id=ticket["movie_session"],
-                                order=order,
-                                row=ticket["row"],
-                                seat=ticket["seat"])
-            new_ticket.save()
+    for ticket in tickets:
+        Ticket.objects.create(movie_session_id=ticket["movie_session"],
+                              order=order,
+                              row=ticket["row"],
+                              seat=ticket["seat"])
 
 
 def get_orders(username: str = None) -> QuerySet[Order]:
