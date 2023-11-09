c100e4d698751d447a97b2645151b8003f7538c9
Rostyslav Chornyi <145897105+RVChornyy@users.noreply.github.com>
2023-11-09 20:21:16 +0200

    Update models.py

--- db/models.py
+++ db/models.py
@@ -111,4 +111,3 @@ class Ticket(models.Model):
                                         force_update,
                                         using,
                                         update_fields)
-
