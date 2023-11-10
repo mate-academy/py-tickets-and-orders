b049e07259ac6f05056c02d9815864321832f871
Serhii <martinenko.sergiy@gmail.com>
2023-11-10 16:14:29 +0200

    fix

--- db/models.py
+++ db/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
+from settings import AUTH_USER_MODEL
 
 
 class Genre(models.Model):
@@ -56,7 +57,7 @@ class MovieSession(models.Model):
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"{self.created_at}"
