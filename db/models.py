a61e6caf14e4b6700615dd44b42488d4e80e0071
Vadym Tiron <tironvadim1@gmail.com>
2023-12-01 19:59:56 +0200

    migrations added

--- db/models.py
+++ db/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+import settings
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -17,7 +19,7 @@ class Actor(models.Model):
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, db_index=True)
     description = models.TextField()
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
@@ -50,3 +52,43 @@ class MovieSession(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"<Order: {self.created_at}>"
+
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey(MovieSession, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    def __str__(self) -> str:
+        return (f"<Ticket: {self.movie_session.movie.title} "
+                f"{self.order.created_at} (row: {self.row}, "
+                f"seat: {self.seat})>")
+
+    def clean(self) -> None:
+        if self.row > ticket.movie_session.cinema_hall.rows:
+            raise ValidationError("row")
+        if self.seat > ticket.movie_session.cinema_hall.seats_in_row:
+            raise ValidationError("seat")
+
+    class Meta:
+        constrains = [
+            models.UniqueConstraint(fields=["row", "seat", "movie_session"])
+        ]
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None) -> None:
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+
+class User(AbstractUser):
+    pass
