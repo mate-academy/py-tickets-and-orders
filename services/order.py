cbe297971b8ae9bb7bcd66e7ba8f2ade1774ff6b
Vadym Tiron <tironvadim1@gmail.com>
2023-12-03 15:06:13 +0200

    migrations added

--- services/order.py
+++ services/order.py
@@ -1,4 +1,5 @@
 from django.db import transaction
+from django.db.models import QuerySet
 
 from db.models import Order, User, Ticket
 
@@ -9,14 +10,13 @@ def create_order(
         username: str,
         date: str = None
 ) -> None:
-    user = User.objects.get(username=username)
+    user = User.objects.filter(username=username)
     order = Order.objects.create(user=user, created_at=date)
     for ticket in tickets:
         Ticket.objects.create(order=order, **ticket)
 
 
-def get_orders(username: str = None) -> models.QuerySet[Order]:
+def get_orders(username: str = None) -> QuerySet[Order]:
     if username:
-        user = User.objects.get(username=username)
-        return Order.objects.filter(user=user)
+        return Order.objects.filter(user__username=username)
     return Order.objects.all()
