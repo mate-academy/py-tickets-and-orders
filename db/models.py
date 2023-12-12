03e62eb5545ed75bb867b2cef9823d9d010fbc13
Hryhoriy <hryhoriy.bilous@gmail.com>
2023-12-12 19:58:34 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -2,18 +2,12 @@ from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
 
-import settings
-
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
         return self.name
-
-
-class User(AbstractUser):
-    pass
 
 
 class Actor(models.Model):
@@ -37,18 +31,6 @@ class Movie(models.Model):
 
     def __str__(self) -> str:
         return self.title
-
-
-class Order(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             on_delete=models.CASCADE)
-
-    class Meta:
-        ordering = ["-created_at"]
-
-    def __str__(self) -> str:
-        return str(self.created_at)
 
 
 class CinemaHall(models.Model):
@@ -77,37 +59,57 @@ class MovieSession(models.Model):
         return f"{self.movie.title} {str(self.show_time)}"
 
 
+class User(AbstractUser):
+    pass
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
 class Ticket(models.Model):
     movie_session = models.ForeignKey(MovieSession, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     row = models.IntegerField()
     seat = models.IntegerField()
 
+    def __str__(self) -> str:
+        return (f"{self.movie_session.movie.title} "
+                f"{self.movie_session.show_time} "
+                f"(row: {self.row}, seat: {self.seat})")
+
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["row", "seat", "movie_session"],
+                fields=["movie_session", "row", "seat"],
                 name="unique_movie_session_row_seat")
         ]
 
-    def __str__(self) -> str:
-        return (f"{self.movie_session.movie} "
-                f"{self.order.created_at} "
-                f"(row: {self.row}, seat: {self.seat})")
-
     def clean(self) -> None:
-        if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
+        if not 1 <= self.row <= self.movie_session.cinema_hall.rows:
             raise ValidationError(
-                {"row": [
-                    f"row number must be in available range:"
-                    f" (1, rows): (1, {self.movie_session.cinema_hall.rows})"]}
+                {
+                    "row": f"row number must be in available range: "
+                    f"(1, rows): "
+                    f"(1, {self.movie_session.cinema_hall.rows})"
+                }
             )
-        if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
+
+        if not 1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row:
             raise ValidationError(
-                {"seat": [
-                    f"seat number must be in available range:"
-                    f" (1, seats_in_row):"
-                    f" (1, {self.movie_session.cinema_hall.seats_in_row})"]}
+                {
+                    "seat": f"seat number must be in available range: "
+                    f"(1, seats_in_row): "
+                    f"(1, "
+                    f"{self.movie_session.cinema_hall.seats_in_row})"
+                }
             )
 
     def save(self,
