06aa0cbd2f1e60b13e2fc64ac5451d2224df3b40
MykolaMazhula <mykm3ua@gmail.com>
2024-01-01 12:44:49 +0100

    Tickets and Orders

--- services/movie_session.py
+++ services/movie_session.py
@@ -42,3 +42,10 @@ def update_movie_session(
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    return list(
+        MovieSession.objects.get(pk=movie_session_id)
+        .tickets.values("row", "seat")
+    )
