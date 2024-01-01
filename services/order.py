4db81219d7ce238a47ad3d2a301cf7a17896b77b
Bohdan Horbachov <bgbro.inc@gmail.com>
2024-01-01 23:26:08 +0200

    completed the task

---
+++ services/order.py
@@ -0,0 +1,44 @@
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Order, Ticket, User
+from datetime import datetime
+
+
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: datetime = None
+) -> Order:
+    with transaction.atomic():
+        user, _ = User.objects.get_or_create(username=username)
+
+        order = Order.objects.create(user=user)
+
+        if date:
+            order.created_at = date
+
+        order.save()
+
+        for ticket_data in tickets:
+            row = ticket_data["row"]
+            seat = ticket_data["seat"]
+            movie_session_id = ticket_data["movie_session"]
+
+            Ticket.objects.create(
+                order=order,
+                row=row,
+                seat=seat,
+                movie_session_id=movie_session_id
+            )
+
+        return order
+
+
+def get_orders(username: str = None) -> QuerySet[Order]:
+    queryset = Order.objects.all()
+
+    if username:
+        queryset = queryset.filter(user__username=username)
+
+    return queryset
