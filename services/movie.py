51962df7905ff982baa675b01964ea190b0c5c05
Yegor Turenko <ytferma@gmail.com>
2024-01-17 11:26:45 +0200

    Solution 2

--- services/movie.py
+++ services/movie.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from django.db import transaction
 from django.db.models import QuerySet, Q
 
@@ -5,9 +7,9 @@ from db.models import Movie
 
 
 def get_movies(
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None,
-    title: str = None
+    genres_ids: list[int] | None = None,
+    actors_ids: list[int] | None = None,
+    title: str | None = None
 ) -> QuerySet:
     queryset = Movie.objects.all()
 
@@ -27,20 +29,20 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
+@transaction.atomic
 def create_movie(
     movie_title: str,
     movie_description: str,
-    genres_ids: list = None,
-    actors_ids: list = None,
+    genres_ids: list[int] | None = None,
+    actors_ids: list[int] | None = None,
 ) -> Movie:
-    with transaction.atomic():
-        movie = Movie.objects.create(
-            title=movie_title,
-            description=movie_description,
-        )
-        if genres_ids:
-            movie.genres.set(genres_ids)
-        if actors_ids:
-            movie.actors.set(actors_ids)
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
 
-        return movie
+    return movie
