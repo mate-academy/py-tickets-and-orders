57f27d722ed7e331bdb59c4513285c7ab6ccb9ae
Denys Sidorov <zondayttop1ggg@gmail.com>
2024-01-23 10:21:40 +0200

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -1,34 +1,30 @@
-from django.db.models import QuerySet
-
-from db.models import MovieSession
+from django.db import transaction
+from db.models import MovieSession, Ticket
 
 
 def create_movie_session(
-    movie_show_time: str, movie_id: int, cinema_hall_id: int
+    show_time: str,
+    movie_id: int,
+    cinema_hall_id: int
 ) -> MovieSession:
-    return MovieSession.objects.create(
-        show_time=movie_show_time,
+    movie_session = MovieSession.objects.create(
+        show_time=show_time,
         movie_id=movie_id,
-        cinema_hall_id=cinema_hall_id,
+        cinema_hall_id=cinema_hall_id
     )
-
-
-def get_movies_sessions(session_date: str = None) -> QuerySet:
-    queryset = MovieSession.objects.all()
-    if session_date:
-        queryset = queryset.filter(show_time__date=session_date)
-    return queryset
+    return movie_session
 
 
 def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
+@transaction.atomic
 def update_movie_session(
     session_id: int,
     show_time: str = None,
     movie_id: int = None,
-    cinema_hall_id: int = None,
+    cinema_hall_id: int = None
 ) -> None:
     movie_session = MovieSession.objects.get(id=session_id)
     if show_time:
@@ -42,3 +38,14 @@ def update_movie_session(
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    tickets = Ticket.objects.filter(movie_session_id=movie_session_id)
+    taken_seats = [
+        {
+            "row": ticket.row,
+            "seat": ticket.seat
+        } for ticket in tickets
+    ]
+    return taken_seats
