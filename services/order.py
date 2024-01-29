13117bd143d8cdfe28c761fb69dae3ba5b05f0ee
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-01-29 19:19:38 +0200

    Solution

--- services/order.py
+++ services/order.py
@@ -5,25 +5,25 @@ from django.db.transaction import atomic
 from db.models import Order, User, Ticket
 
 
+@atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = None
 ) -> Order:
     user_ = User.objects.get(username=username)
-    with atomic():
-        order = Order.objects.create(user=user_)
-        if date:
-            order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
-            order.save()
-        for ticket in tickets:
-            if ticket:
-                Ticket.objects.create(
-                    movie_session_id=ticket["movie_session"],
-                    row=ticket["row"],
-                    seat=ticket["seat"],
-                    order_id=order.id,
-                )
+    order = Order.objects.create(user=user_)
+    if date:
+        order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+        order.save()
+    for ticket in tickets:
+        if ticket:
+            Ticket.objects.create(
+                movie_session_id=ticket["movie_session"],
+                row=ticket["row"],
+                seat=ticket["seat"],
+                order_id=order.id,
+            )
     return order
 
 
