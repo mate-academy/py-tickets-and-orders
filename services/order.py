0dcb8c16ced6e5cdbb3e6dc559f0aa008953509e
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-02-09 21:42:55 +0200

    To transaction decorator and fixed conditions

--- services/order.py
+++ services/order.py
@@ -6,40 +6,40 @@ from db.models import Ticket, Order, User
 from services.movie_session import get_movie_session_by_id
 
 
+@transaction.atomic
 def create_order(
     tickets: list[dict],
     username: str,
     date: datetime.date = None
 ) -> None:
-    with transaction.atomic():
-        user = User.objects.get(username=username)
+    user = User.objects.get(username=username)
 
-        additional_order_data = {"user": user}
+    additional_order_data = {"user": user}
 
-        order = Order.objects.create(**additional_order_data)
+    order = Order.objects.create(**additional_order_data)
 
-        if date:
-            order.created_at = date
+    if date:
+        order.created_at = date
 
-        order.save()
+    order.save()
 
-        ticket_objects = [
-            Ticket(
-                row=ticket_data["row"],
-                seat=ticket_data["seat"],
-                movie_session=get_movie_session_by_id(
-                    ticket_data["movie_session"]
-                ),
-                order=order
-            )
-            for ticket_data in tickets
-        ]
+    ticket_objects = [
+        Ticket(
+            row=ticket_data["row"],
+            seat=ticket_data["seat"],
+            movie_session=get_movie_session_by_id(
+                ticket_data["movie_session"]
+            ),
+            order=order
+        )
+        for ticket_data in tickets
+    ]
 
-        Ticket.objects.bulk_create(ticket_objects)
+    Ticket.objects.bulk_create(ticket_objects)
 
 
 def get_orders(username: str = None) -> QuerySet:
-    if username is not None:
+    if username:
         return Order.objects.filter(user__username=username)
 
     return Order.objects.all()
--- services/user.py
+++ services/user.py
@@ -10,11 +10,11 @@ def create_user(
 ) -> User:
     user = User.objects.create_user(username=username, password=password)
 
-    if email is not None:
+    if email:
         user.email = email
-    if first_name is not None:
+    if first_name:
         user.first_name = first_name
-    if last_name is not None:
+    if last_name:
         user.last_name = last_name
 
     user.save()
@@ -36,15 +36,15 @@ def update_user(
 ) -> User:
     user = get_user(user_id)
 
-    if username is not None:
+    if username:
         user.username = username
-    if email is not None:
+    if email:
         user.email = email
-    if password is not None:
+    if password:
         user.set_password(password)
-    if first_name is not None:
+    if first_name:
         user.first_name = first_name
-    if last_name is not None:
+    if last_name:
         user.last_name = last_name
 
     user.save()
