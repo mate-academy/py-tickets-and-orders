d343ad93b3e04cd8cb0519eb8bc50c8ec4b7348f
el chibuk <chebukin404@gmail.com>
2024-02-13 17:00:46 +0200

    fixed user issue

--- services/user.py
+++ services/user.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from django.contrib.auth import get_user_model
+
 from db.models import User
 
 
@@ -10,7 +12,7 @@ def create_user(
         first_name: Optional[str] = None,
         last_name: Optional[str] = None
 ) -> User:
-    user = User.objects.create_user(
+    user = get_user_model().objects.create_user(
         username=username, password=password,
     )
 
@@ -26,7 +28,7 @@ def create_user(
 
 
 def get_user(user_id: int) -> User:
-    return User.objects.get(pk=user_id)
+    return get_user_model().objects.get(pk=user_id)
 
 
 def update_user(
@@ -37,7 +39,7 @@ def update_user(
         first_name: Optional[str] = None,
         last_name: Optional[str] = None,
 ) -> None:
-    user = User.objects.get(pk=user_id)
+    user = get_user_model().objects.get(pk=user_id)
 
     if username:
         user.username = username
