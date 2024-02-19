63897c1335431057b42ec7b9b8c795a485412965
Maksym Symonovych <maksym.symonovych@gmail.com>
2024-02-19 14:58:55 +0200

    fix services/movie_session.py

--- services/movie_session.py
+++ services/movie_session.py
@@ -45,9 +45,8 @@ def delete_movie_session_by_id(session_id: int) -> None:
 
 
 def get_taken_seats(movie_session_id: int) -> list[dict]:
-    return [
-        tickets
-        for tickets in MovieSession.objects.get(
+    return list(
+        MovieSession.objects.get(
             id=movie_session_id
         ).tickets.values("row", "seat")
-    ]
+    )
