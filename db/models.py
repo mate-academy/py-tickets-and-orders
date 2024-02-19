738c6e7db21a730e27be1609c079f79db5be6c3d
Andrii Zubchenko <dissom1987@gmail.com>
2024-02-19 21:37:19 +0200

    TEST

--- db/models.py
+++ db/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.forms import ValidationError
+import settings
 
 
 class Genre(models.Model):
@@ -25,6 +28,9 @@ class Movie(models.Model):
     def __str__(self) -> str:
         return self.title
 
+    class Meta:
+        indexes = [models.Index(fields=["title"])]
+
 
 class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
@@ -50,3 +56,52 @@ class MovieSession(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"Order: {self.create_at}"
+
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey(MovieSession, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    def clean(self) -> None:
+        if not (1 <= self.row <= self.ticket.movie_session.cinema_hall.rows):
+            raise ValidationError(
+                f"Row must be in range "
+                f"[1, {self.movie_session.cinema_hall.num_rows}], "
+                f"not {self.row}"
+                )
+        if not (1 <= self.seat <= self.ticket.movie_session.cinema_hall.seats):
+            raise ValidationError(
+                f"Seat must be in range "
+                f"[1, {self.movie_session.cinema_hall.seats}], "
+                f"not {self.row}"
+            )
+
+    def save(self, *args, **kwargs) -> None:
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["row", "seat", "movie_session"],
+                name = "unique_ticket"
+            )
+            ]
+
+    def __str__(self) -> str:
+        return f"Ticket: speed {self.order.create_at} ({self.row}, {self.seat})"
+
+
+class User(AbstractUser):
+    pass
