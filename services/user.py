1104c5d3f9990d71f458d7aca67d4322a748dc10
Serhii Buryk <87121473+Serejka21@users.noreply.github.com>
2024-02-20 15:54:31 +0200

    Updated get_user method

--- services/user.py
+++ services/user.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 from db.models import User
 
 
@@ -23,7 +25,7 @@ def create_user(username: str,
 
 
 def get_user(user_id: int) -> User:
-    return User.objects.get(id=user_id)
+    return get_user_model().objects.get(pk=user_id)
 
 
 def update_user(user_id: int,
@@ -33,7 +35,7 @@ def update_user(user_id: int,
                 first_name: str | None = None,
                 last_name: str | None = None) -> None:
 
-    user = User.objects.get(id=user_id)
+    user = get_user(user_id)
 
     if username:
         user.username = username
