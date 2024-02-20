bc77a6dbfcb377eb8fe9dfbd3972cc5a02bf9f2f
PythonZem <pythonzem@gmail.com>
2024-02-20 22:18:30 +0200

    fix imports

--- db/models.py
+++ db/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+
 from django.core.exceptions import ValidationError
+
 from django.db import models
 
-import settings
+from django.conf import settings
 
 
 class Genre(models.Model):
