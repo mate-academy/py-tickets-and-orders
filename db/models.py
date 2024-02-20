d7684482903bb523199be594115f4d5d8e5c6896
Yevhen <evgeniy.playid@icloud.com>
2024-02-21 00:41:55 +0200

    'Solution'

--- db/models.py
+++ db/models.py
@@ -100,10 +100,11 @@ class Ticket(models.Model):
             )
         if not 1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row:
             raise ValidationError(
-                {"seat": [
-                    "seat number must be in available range: "
-                    "(1, seats_in_row):"
-                    f" {(1, self.movie_session.cinema_hall.seats_in_row)}"]
+                {
+                    "seat": [
+                        "seat number must be in available range: "
+                        "(1, seats_in_row):"
+                        f" {(1, self.movie_session.cinema_hall.seats_in_row)}"]
                 }
             )
 
