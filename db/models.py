ea5c90cd2fcb65eabbad2923778503e13b3ffc76
Kyrylo Storozhenko <155245819+kstorozhenko@users.noreply.github.com>
2024-02-21 11:57:51 +0200

    Update db/models.py
    Co-authored-by: Yaroslav <104159378+Y-Havryliv@users.noreply.github.com>

--- db/models.py
+++ db/models.py
@@ -88,7 +88,7 @@ class Ticket(models.Model):
         if not 1 <= self.row <= self.movie_session.cinema_hall.rows:
             raise ValidationError(
                 {"row": "row number must be in available range: "
-                 f"(1, rows): "
+                 "(1, rows): "
                  f"(1, {self.movie_session.cinema_hall.rows})"}
             )
         if not 1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row:
