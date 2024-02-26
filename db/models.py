99847075888ba6f384840359467f7dc5b05cf741
Kyrylo Storozhenko <busterakel@gmail.com>
2024-02-26 13:43:39 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -76,7 +76,11 @@ class Order(models.Model):
 class Ticket(models.Model):
     movie_session = models.ForeignKey(
         MovieSession, on_delete=models.CASCADE, related_name="tickets")
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="tickets")
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
     row = models.IntegerField()
     seat = models.IntegerField()
 
