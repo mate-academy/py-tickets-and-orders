ffaa3e1503172f848d3bb2976a203cb2142a7bad
Molchanov Ihor <IMolchanov@kdmg.com.ua>
2024-03-04 18:14:33 +0200

    fixed flake8 errors

--- settings.py
+++ settings.py
@@ -26,22 +26,7 @@ USE_TZ = False
 AUTH_USER_MODEL = "db.User"
 
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
     "db",
-]
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
 ]
