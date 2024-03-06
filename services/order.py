0ed445acc7b37e662e1c5a2e0f45f293c0afa71a
Artem Chernyshov <achernyshovv1@gmail.com>
2024-03-06 08:57:09 +0200

    Solution

--- services/order.py
+++ services/order.py
@@ -1,35 +1,37 @@
 from datetime import date
-
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
-
 from db.models import Order, Ticket
 
 
+def transactional_function(func):
+    def wrapper(*args, **kwargs):
+        with transaction.atomic():
+            return func(*args, **kwargs)
+    return wrapper
+
+
+@transactional_function
 def create_order(
         tickets: list[dict],
         username: str,
         date: date | None = None
 ) -> None:
-    with transaction.atomic():
-        user = get_user_model().objects.get(
-            username=username
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
+
+    if date:
+        order.created_at = date
+        order.save()
+
+    for ticket in tickets:
+        Ticket.objects.create(
+            order=order,
+            row=ticket["row"],
+            seat=ticket["seat"],
+            movie_session_id=ticket["movie_session"]
         )
-
-        order = Order.objects.create(user=user)
-
-        if date:
-            order.created_at = date
-            order.save()
-
-        for ticket in tickets:
-            Ticket.objects.create(
-                order=order,
-                row=ticket["row"],
-                seat=ticket["seat"],
-                movie_session_id=ticket["movie_session"]
-            )
 
 
 def get_orders(
