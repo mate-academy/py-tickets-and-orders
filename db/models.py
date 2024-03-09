ce16d3877ae14feefb6c8a41ee8cfa3c7c639f12
Aeros72 <pashamelnyk2004@gmail.com>
2024-03-09 18:27:07 +0200

    Solution!

--- db/models.py
+++ db/models.py
@@ -93,9 +93,9 @@ class Ticket(models.Model):
         ]
 
     def __str__(self) -> str:
-        return f"{self.movie_session.movie.title} "\
-               f"{self.movie_session.show_time} "\
-               f"(row: {self.row}, seat: {self.seat})"
+        return (f"{self.movie_session.movie.title} "
+                f"{self.movie_session.show_time} "
+                f"(row: {self.row}, seat: {self.seat})")
 
     def clean(self) -> None:
         if self.row not in range(1, self.movie_session.cinema_hall.rows + 1):
