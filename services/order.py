d1b5743846a3573eb898b81719719bcfb7915e58
Vadim Babii <judviiioff@gmail.com>
2024-03-09 20:57:56 +0200

    used the "transaction.atomic" decorator for better readability

--- services/order.py
+++ services/order.py
@@ -4,31 +4,31 @@ from django.db.models import QuerySet
 from django.db import transaction
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = None
 ) -> Order:
-    with transaction.atomic():
-        user = get_user_model().objects.get(username=username)
-        order = Order.objects.create(user=user)
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
 
-        if date:
-            order.created_at = date
+    if date:
+        order.created_at = date
 
-        order.save()
+    order.save()
 
-        for ticket in tickets:
-            row, seat, movie_session_id = ticket.values()
-            movie_session = MovieSession.objects.get(pk=movie_session_id)
-            Ticket.objects.create(
-                movie_session=movie_session,
-                order=order,
-                row=row,
-                seat=seat
-            )
+    for ticket in tickets:
+        row, seat, movie_session_id = ticket.values()
+        movie_session = MovieSession.objects.get(pk=movie_session_id)
+        Ticket.objects.create(
+            movie_session=movie_session,
+            order=order,
+            row=row,
+            seat=seat
+        )
 
-        return order
+    return order
 
 
 def get_orders(username: str = None) -> QuerySet:
