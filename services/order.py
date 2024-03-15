759155f446520431fac354f9d90eca1a4677eba4
dmytro-yefimenko <dmytro@yefimenko.uk>
2024-03-15 20:43:19 +0200

    Requested fixed applied

--- services/order.py
+++ services/order.py
@@ -9,7 +9,7 @@ def create_order(
         tickets: list[dict],
         username: str,
         date: str | None = None
-) -> None:
+) -> Order:
     order = Order.objects.create(
         user=User.objects.get(username=username)
     )
@@ -29,7 +29,7 @@ def create_order(
     return order
 
 
-def get_orders(username: str | None = None) -> QuerySet:
+def get_orders(username: str | None = None) -> QuerySet[Order]:
     if username:
         return Order.objects.filter(user__username=username)
     return Order.objects.all()
