71fbc6df858189e5055ec1a38fb46ffbd98b2212
Yura Push <youdeimos1@gmail.com>
2024-03-20 15:55:21 +0200

    -used transaction.atomic descriptor -some code style changes

--- services/order.py
+++ services/order.py
@@ -5,25 +5,25 @@ from django.db.models import QuerySet
 from db.models import Order, Ticket
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = None
 ) -> Order:
-    with transaction.atomic():
-        user = get_user_model().objects.get(username=username)
-        order = Order.objects.create(user=user)
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
 
-        for ticket in tickets:
-            Ticket.objects.create(
-                movie_session_id=ticket["movie_session"],
-                order_id=order.id,
-                row=ticket["row"],
-                seat=ticket["seat"]
-            )
+    for ticket in tickets:
+        Ticket.objects.create(
+            movie_session_id=ticket["movie_session"],
+            order_id=order.id,
+            row=ticket["row"],
+            seat=ticket["seat"]
+        )
 
-        if date:
-            order.created_at = date
+    if date:
+        order.created_at = date
     order.save()
     return order
 
