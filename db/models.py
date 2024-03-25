09519b9f8d86bce34d866bf0fa0f8d34e644c7f7
Dmitro Sysoev <dimasysoev20000@gmail.com>
2024-03-25 21:21:09 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.exceptions import ValidationError
+import settings
 
 
 class Genre(models.Model):
@@ -25,6 +28,9 @@ class Movie(models.Model):
     def __str__(self) -> str:
         return self.title
 
+    class Meta:
+        indexes = [models.Index(fields=["title"])]
+
 
 class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
@@ -50,3 +56,50 @@ class MovieSession(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.created_at}"
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey(to=MovieSession,
+                                      on_delete=models.CASCADE)
+    order = models.ForeignKey(to=Order, on_delete=models.CASCADE)
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.movie_session.movie.title} \
+         {self.movie_session.show_time} (row: {self.row}, seat: {self.seat})"
+
+    class Meta:
+        constraints = [models.UniqueConstraint(
+            fields=["row", "seat", "movie_session"],
+            name="unique_row_seat_movie_session")]
+
+    def clean(self) -> None:
+        if self.row > self.movie_session.cinema_hall \
+           or self.seat > self.movie_session:
+            raise ValidationError(
+                {"row": [
+                    "row number must be in available range: "
+                    f"(1, rows): (1, "
+                    f"{self.movie_session.cinema_hall.rows})"]}
+            )
+
+    def save(self, *args, **kwargs) -> None:
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+
+class User(AbstractUser):
+    pass
---
+++ services/order.py
@@ -0,0 +1,33 @@
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Order, Ticket, User
+
+
+@transaction.atomic
+def create_order(tickets: list[dict],
+                 movie_session: int,
+                 username: str,
+                 date: str = None) -> Order:
+    order = Order.objects.create(
+        user=User.objects.get(username=username)
+    )
+
+    if date:
+        order.date = date
+        order.save()
+
+    for ticket in tickets:
+        Ticket.objects.create(
+            movie_session_id=ticket.get("movie_session"),
+            order_id=order.id,
+            row=ticket.get("row"),
+            seat=ticket.get("seat")
+        )
+    return order
+
+
+def get_order(username: str) -> QuerySet[Order]:
+    if username:
+        Order.objects.filter(user__username=username)
+    Order.objects.all()
