01bdeec25e80630dc39d1b9ff0801861c5ecd802
sergey <38cerg12021979@jmal.com>
2024-03-28 14:18:05 +0200

    28_03_24_01

--- services/user.py
+++ services/user.py
@@ -37,7 +37,7 @@ def update_user(
         first_name: str = None,
         last_name: str = None
 ) -> get_user_model():
-    user = get_user_model().objects.get(pk=user_id)
+    user = get_user(user_id)
 
     if username:
         user.username = username
