75581caf6246a38f00f6c91def5d5b70fcc7e26c
Ihor Dmytriv <dmytriwihor@gmail.com>
2024-03-31 17:37:24 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -66,7 +66,7 @@ class Order(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self) -> str:
-        return f"Order: {self.created_at}"
+        return str(self.created_at)
 
 
 class Ticket(models.Model):
@@ -85,23 +85,22 @@ class Ticket(models.Model):
 
     def __str__(self) -> str:
         return (
-            f"Ticket: {self.movie_session.movie.title} "
-            f"{self.order.created_at} "
+            f"{self.movie_session.movie.title} "
+            f"{self.movie_session.show_time} "
             f"(row: {self.row}, seat: {self.seat})"
         )
 
     def clean(self) -> None:
         if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
             raise ValidationError(
-                {"row": f"row must be in range "
-                 f"[1, {self.movie_session.cinema_hall.rows}], "
-                 f"not {self.row}"}
+                {"row": "row number must be in available range: (1, rows): "
+                 f"(1, {self.movie_session.cinema_hall.rows})"}
             )
         if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
             raise ValidationError(
-                {"seat": f"seat must be in range "
-                 f"[1, {self.movie_session.cinema_hall.seats_in_row}], "
-                 f"not {self.seat}"}
+                {"seat": "seat number must be in available range: "
+                 "(1, seats_in_row): "
+                 f"(1, {self.movie_session.cinema_hall.seats_in_row})"}
             )
 
     def save(self, *args, **kwargs) -> None:
