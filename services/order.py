8a67f29c241bbf47dced07c89686c9ae054dc26b
Vitalij Chajka <chaika.vitalii@gmail.com>
2024-04-02 21:36:49 +0300

    Solution: made changes according to comment

--- services/order.py
+++ services/order.py
@@ -4,37 +4,35 @@ from datetime import datetime
 from db.models import Ticket, Order, User, MovieSession
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = datetime.now().strftime("%Y-%m-%d %H:%M"),
 ) -> None:
-    with transaction.atomic():
-        new_order = Order(
-            user=User.objects.get(username=username),
-            created_at=datetime.strptime(date, "%Y-%m-%d %H:%M")
-        )
-        print(new_order.created_at)
-        new_order.save()
-        print(new_order.created_at)
-        Ticket.objects.bulk_create(
-            [
-                Ticket(
-                    movie_session=MovieSession.objects.get(
-                        id=ticket["movie_session"]
-                    ),
-                    order=new_order,
-                    row=ticket["row"],
-                    seat=ticket["seat"]
-                ) for ticket in tickets
-            ]
-        )
+    new_order = Order(
+        user=User.objects.get(username=username),
+        created_at=datetime.strptime(date, "%Y-%m-%d %H:%M")
+    )
+    print(new_order.created_at)
+    new_order.save()
+    print(new_order.created_at)
+    Ticket.objects.bulk_create(
+        [
+            Ticket(
+                movie_session=MovieSession.objects.get(
+                    id=ticket["movie_session"]
+                ),
+                order=new_order,
+                row=ticket["row"],
+                seat=ticket["seat"]
+            ) for ticket in tickets
+        ]
+    )
 
 
 def get_orders(username: str = None) -> list[Order]:
     queryset = Order.objects.all()
-    if username is not None:
-        queryset = queryset.filter(
-            user=User.objects.get(username=username)
-        ).values_list("id",)
+    if username:
+        queryset = queryset.filter(user__username=username)
     return queryset
