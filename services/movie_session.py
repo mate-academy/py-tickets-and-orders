9662bbb56fd33cbaf3793c7c3620196856fdae42
Kateryna Semesenko <semesenkoekaterina@gmail.com>
2024-04-03 10:27:04 +0300

    Fixed Tickets and orders

--- services/movie_session.py
+++ services/movie_session.py
@@ -12,7 +12,7 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet:
+def get_movies_sessions(session_date: str | None = None) -> QuerySet:
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
@@ -25,9 +25,9 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 def update_movie_session(
     session_id: int,
-    show_time: str = None,
-    movie_id: int = None,
-    cinema_hall_id: int = None,
+    show_time: str | None = None,
+    movie_id: int | None = None,
+    cinema_hall_id: int | None = None,
 ) -> None:
     movie_session = MovieSession.objects.get(id=session_id)
     if show_time:
--- services/order.py
+++ services/order.py
@@ -5,32 +5,32 @@ from django.contrib.auth import get_user_model
 from datetime import datetime
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
-        date: str = None
+        date: str | None = None
 ) -> Order:
 
-    with transaction.atomic():
-        order = Order(user=get_user_model().objects.get(username=username))
-        order.save()
-        if date is not None:
-            order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
-        order.save()
+    order = Order(user=get_user_model().objects.get(username=username))
+    order.save()
+    if date is not None:
+        order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+    order.save()
 
-        for ticket in tickets:
-            ticket = Ticket(
-                order=order,
-                movie_session_id=ticket["movie_session"],
-                row=ticket["row"],
-                seat=ticket["seat"],
-            )
-            ticket.save()
+    for ticket in tickets:
+        ticket = Ticket(
+            order=order,
+            movie_session_id=ticket["movie_session"],
+            row=ticket["row"],
+            seat=ticket["seat"],
+        )
+        ticket.save()
 
     return order
 
 
-def get_orders(username: str = None) -> List[Order]:
+def get_orders(username: str | None = None) -> List[Order]:
     orders = Order.objects.all()
     if username is not None:
         orders = orders.filter(user__username=username)
