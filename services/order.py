fd33e479ec8eb63f708c02e2575e845ec353bda3
antonver <anton.verbovyi@gmail.com>
2024-04-09 23:40:08 +0200

    Done

--- services/order.py
+++ services/order.py
@@ -7,30 +7,30 @@ from django.db.models import QuerySet
 from db.models import Order, Ticket, MovieSession
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = None
 ) -> None:
-    with transaction.atomic():
-        order = Order.objects.create(
-            user=get_user_model().objects.get(username=username)
-        )
-        if date:
-            order.created_at = date
-            order.save()
-        Ticket.objects.bulk_create(
-            [
-                Ticket(
-                    order=order,
-                    row=ticket["row"],
-                    seat=ticket["seat"],
-                    movie_session=MovieSession.objects.get(
-                        id=ticket["movie_session"])
-                )
-                for ticket in tickets
-            ]
-        )
+    order = Order.objects.create(
+        user=get_user_model().objects.get(username=username)
+    )
+    if date:
+        order.created_at = date
+        order.save()
+    Ticket.objects.bulk_create(
+        [
+            Ticket(
+                order=order,
+                row=ticket["row"],
+                seat=ticket["seat"],
+                movie_session=MovieSession.objects.get(
+                    id=ticket["movie_session"])
+            )
+            for ticket in tickets
+        ]
+    )
 
 
 def get_orders(username: str = None) -> Order | QuerySet[Order]:
