ffd1db52ac85303c48a78f61b4dc6517c88059c3
antonver <anton.verbovyi@gmail.com>
2024-04-09 20:24:55 +0200

    Done

--- db/models.py
+++ db/models.py
@@ -97,8 +97,8 @@ class Ticket(models.Model):
         if self.row > self.movie_session.cinema_hall.rows:
             raise ValidationError(
                 {"row": [f"row number must be in available range: "
-                      f"(1, rows): "
-                      f"(1, {self.movie_session.cinema_hall.rows})"]})
+                         f"(1, rows): "
+                         f"(1, {self.movie_session.cinema_hall.rows})"]})
 
         if self.seat > self.movie_session.cinema_hall.seats_in_row:
             raise ValidationError({"seat": [
