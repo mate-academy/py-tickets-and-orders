ffbb11680fd286b1391b32fcb83852683af9bb45
donbas777 <renary34@gmail.com>
2024-04-10 23:09:49 +0300

    'Solution'

--- services/order.py
+++ services/order.py
@@ -6,25 +6,25 @@ from db.models import Ticket
 from db.models import User
 
 
+@transaction.atomic
 def create_order(tickets: list[dict], username: str, date: str = None) -> None:
-    with transaction.atomic():
-        user = User.objects.get(username=username)
-        order = Order.objects.create(user=user)
-        if date:
-            order.created_at = date
-        order.save()
+    user = User.objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = date
+    order.save()
 
-        for ticket in tickets:
-            Ticket.objects.create(
-                movie_session_id=ticket["movie_session"],
-                order=order,
-                row=ticket["row"],
-                seat=ticket["seat"]
-            )
+    for ticket in tickets:
+        Ticket.objects.create(
+            movie_session_id=ticket["movie_session"],
+            order=order,
+            row=ticket["row"],
+            seat=ticket["seat"]
+        )
 
 
 def get_orders(username: str = None) -> QuerySet:
+    queryset = Order.objects.all()
     if username:
-        return Order.objects.filter(user__username=username)
-
-    return Order.objects.all()
+        queryset = queryset.filter(user__username=username)
+    return queryset
