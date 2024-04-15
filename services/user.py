ec8c8f2858ab376200bcce873e04747a09f921c4
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2024-04-15 19:16:09 +0300

    Final_solution_1

---
+++ services/user.py
@@ -0,0 +1,56 @@
+from db.models import User
+from typing import Optional
+
+
+def create_user(
+        username: str,
+        password: str,
+        email: Optional[str] = None,
+        first_name: Optional[str] = None,
+        last_name: Optional[str] = None
+) -> User:
+    user: User = User.objects.create_user(
+        username=username,
+        password=password
+    )
+    if email:
+        user.email = email
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
+    user.save()
+    return user
+
+
+def get_user(user_id: int) -> Optional[User]:
+    try:
+        return User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        return None
+
+
+def update_user(
+        user_id: int,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        email: Optional[str] = None,
+        first_name: Optional[str] = None,
+        last_name: Optional[str] = None
+) -> Optional[User]:
+    try:
+        user: User = User.objects.get(pk=user_id)
+        if username:
+            user.username = username
+        if password:
+            user.set_password(password)
+        if email:
+            user.email = email
+        if first_name:
+            user.first_name = first_name
+        if last_name:
+            user.last_name = last_name
+        user.save()
+        return user
+    except User.DoesNotExist:
+        return None
