e9ec849345cc03ed5b35f12a84c4fe032d8e8fbf
dmitriy kim <d.villarionovich@gmail.com>
2024-04-26 13:28:41 +0200

    solution

--- services/user.py
+++ services/user.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 import init_django_orm  # noqa: F401
 from db.models import User
 
@@ -21,7 +23,7 @@ def create_user(username: str,
 
 
 def get_user(user_id: int) -> User:
-    return User.objects.get_user_model(pk=user_id)
+    return get_user_model().objects.get(pk=user_id)
 
 
 def update_user(user_id: int,
@@ -30,7 +32,7 @@ def update_user(user_id: int,
                 email: str = None,
                 first_name: str = None,
                 last_name: str = None) -> User:
-    user = User.objects.get_user_model(pk=user_id)
+    user = get_user_model().objects.get(pk=user_id)
     if username is not None:
         user.username = username
     if password is not None:
