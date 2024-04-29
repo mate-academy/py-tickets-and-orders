f56696c6927989e9922d1624a47e69d528dfdf80
Mykhailo Yemets <mikhailyemets.pl@gmail.com>
2024-04-29 17:14:24 +0200

    added some small fix to annotations

--- services/user.py
+++ services/user.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from django.contrib.auth import get_user_model
 
 from db.models import User
@@ -6,9 +8,9 @@ from db.models import User
 def create_user(
         username: str,
         password: str,
-        email: str = None,
-        first_name: str = None,
-        last_name: str = None
+        email: Optional[str] = None,
+        first_name: Optional[str] = None,
+        last_name: Optional[str] = None
 ) -> None:
     user = get_user_model().objects.create_user(
         username=username, password=password
