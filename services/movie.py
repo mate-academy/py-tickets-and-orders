f1627c8a6a7158bc7a3bd37f4b3289f3d0d3e5f6
Dmytro Pukas <pukasdima2002@gmail.com>
2024-04-29 09:31:14 +0200

    Fixed indentations

--- services/movie.py
+++ services/movie.py
@@ -7,9 +7,9 @@ from db.models import Movie
 
 
 def get_movies(
-        genres_ids: Optional[list] = None,
-        actors_ids: Optional[list] = None,
-        title: Optional[str] = None,
+    genres_ids: Optional[list] = None,
+    actors_ids: Optional[list] = None,
+    title: Optional[str] = None,
 ) -> QuerySet:
     queryset = Movie.objects.all()
 
@@ -31,10 +31,10 @@ def get_movie_by_id(movie_id: int) -> Movie:
 
 @transaction.atomic
 def create_movie(
-        movie_title: str,
-        movie_description: str,
-        genres_ids: Optional[list] = None,
-        actors_ids: Optional[list] = None,
+    movie_title: str,
+    movie_description: str,
+    genres_ids: Optional[list] = None,
+    actors_ids: Optional[list] = None,
 ) -> Movie:
     movie = Movie.objects.create(
         title=movie_title,
