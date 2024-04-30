64d726277ac43ea264a823429fbaec78557a5a8b
semermaks <semermaks@mail.com>
2024-04-30 10:34:01 +0300

    Use get_user_model() and Optional and small fixes

--- services/order.py
+++ services/order.py
@@ -1,29 +1,31 @@
+from typing import Optional
+
+from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
 
-from db.models import Order
-from db.models import Ticket
-from db.models import User
+from db.models import Ticket, Order
 
 
 @transaction.atomic
 def create_order(tickets: list[dict], username: str, date: str = None) -> None:
-    user = User.objects.get(username=username)
-    order = Order.objects.create(user=user)
-    if date:
-        order.created_at = date
-    order.save()
+    with transaction.atomic():
+        user_id = get_user_model().objects.get(username=username).id
+        order = Order.objects.create(user_id=user_id)
+        if date:
+            order.created_at = date
+            order.save()
 
-    for ticket in tickets:
-        Ticket.objects.create(
-            movie_session_id=ticket["movie_session"],
-            order=order,
-            row=ticket["row"],
-            seat=ticket["seat"]
-        )
+        for ticket in tickets:
+            Ticket.objects.create(
+                order_id=order.id,
+                row=ticket["row"],
+                seat=ticket["seat"],
+                movie_session_id=ticket["movie_session"]
+            )
 
 
-def get_orders(username: str = None) -> QuerySet[str]:
+def get_orders(username: Optional[str] = None) -> QuerySet[Order]:
     queryset = Order.objects.all()
     if username:
         queryset = queryset.filter(user__username=username)
