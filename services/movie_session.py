cd49b46bf435ac2287e312a5c00bb19fa8703f62
Alona Sorochynska <alona.sorochynska@gmail.com>
2024-05-02 07:57:44 +0100

    flake8)

--- services/movie_session.py
+++ services/movie_session.py
@@ -15,7 +15,9 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: Optional[str] = None) -> QuerySet[MovieSession]:
+def get_movies_sessions(
+        session_date: Optional[str] = None
+) -> QuerySet[MovieSession]:
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
