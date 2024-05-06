73b987e94d09f03dd19aac27edc20a3b1727f40e
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-05-06 17:49:39 +0300

    Solution3

--- services/movie.py
+++ services/movie.py
@@ -7,15 +7,14 @@ from db.models import Movie
 
 
 def get_movies(
-        genres_ids: Optional[list[int]] = None,
-        actors_ids: Optional[list[int]] = None,
-        title: Optional[str] = None
+    genres_ids: Optional[list[int]] = None,
+    actors_ids: Optional[list[int]] = None,
+    title: Optional[str] = None
 ) -> QuerySet:
     queryset = Movie.objects.all()
 
     if genres_ids:
         queryset = queryset.filter(genres__id__in=genres_ids)
-
     if actors_ids:
         queryset = queryset.filter(actors__id__in=actors_ids)
 
@@ -31,10 +30,10 @@ def get_movie_by_id(movie_id: int) -> Movie:
 
 @transaction.atomic
 def create_movie(
-        movie_title: str,
-        movie_description: str,
-        genres_ids: Optional[list[int]] = None,
-        actors_ids: Optional[list[int]] = None,
+    movie_title: str,
+    movie_description: str,
+    genres_ids: Optional[list[int]] = None,
+    actors_ids: Optional[list[int]] = None
 ) -> Movie:
     movie = Movie.objects.create(
         title=movie_title,
@@ -44,5 +43,4 @@ def create_movie(
         movie.genres.set(genres_ids)
     if actors_ids:
         movie.actors.set(actors_ids)
-
     return movie
--- services/movie_session.py
+++ services/movie_session.py
@@ -6,7 +6,7 @@ from db.models import MovieSession, Ticket
 
 
 def create_movie_session(
-    movie_show_time: str, movie_id: int, cinema_hall_id: int
+        movie_show_time: str, movie_id: int, cinema_hall_id: int
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
@@ -27,10 +27,10 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 
 def update_movie_session(
-    session_id: int,
-    show_time: Optional[str] = None,
-    movie_id: Optional[int] = None,
-    cinema_hall_id: Optional[int] = None,
+        session_id: int,
+        show_time: Optional[str] = None,
+        movie_id: Optional[int] = None,
+        cinema_hall_id: Optional[int] = None,
 ) -> None:
     movie_session = MovieSession.objects.get(id=session_id)
     if show_time:
@@ -47,8 +47,6 @@ def delete_movie_session_by_id(session_id: int) -> None:
 
 
 def get_taken_seats(movie_session_id: int) -> list:
-    return list(
-        Ticket.objects.filter(
-            movie_session_id=movie_session_id
-        ).values("row", "seat")
-    )
+    return list(Ticket.objects.filter(
+        movie_session_id=movie_session_id
+    ).values("row", "seat"))
