181cc1308372b2c36989f1a7fb9700c5094cc419
haldaniko <haldaniko@gmail.com>
2024-05-18 16:16:30 +0200

    add decorators

--- services/order.py
+++ services/order.py
@@ -5,37 +5,31 @@ from django.db.models import QuerySet
 from db.models import Order, User, Ticket, MovieSession
 
 
-def create_order(
-        tickets: list[dict],
-        username: str,
-        date: str = None
-) -> Order:
-    with transaction.atomic():
-        try:
-            user = get_user_model().objects.get(username=username)
-        except User.DoesNotExist:
-            raise ValueError(f"There is no user with username '{username}'")
+@transaction.atomic
+def create_order(tickets: list[dict], username: str, date: str = None) -> Order:
+    try:
+        user = get_user_model().objects.get(username=username)
+    except get_user_model().DoesNotExist:
+        raise ValueError(f"There is no user with username '{username}'")
 
-        order = Order.objects.create(user=user)
+    order = Order.objects.create(user=user)
 
-        tickets = [
-            Ticket(
-                order=order,
-                row=data["row"],
-                seat=data["seat"],
-                movie_session=MovieSession.objects.get(
-                    id=data["movie_session"]
-                )
-            )
-            for data in tickets
-        ]
+    tickets = [
+        Ticket(
+            order=order,
+            row=data["row"],
+            seat=data["seat"],
+            movie_session=MovieSession.objects.get(id=data["movie_session"])
+        )
+        for data in tickets
+    ]
 
-        if date:
-            order.created_at = date
-            order.save()
+    if date:
+        order.created_at = date
+        order.save()
 
-        Ticket.objects.bulk_create(tickets)
-        return order
+    Ticket.objects.bulk_create(tickets)
+    return order
 
 
 def get_orders(username: str = None) -> QuerySet[Order]:
