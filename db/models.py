52a14a9d7a78c4acef04f7e8427273740325910e
Oles Shulzhenko <simplemanua@gmail.com>
2024-05-23 11:57:54 +0300

    fix flake8

--- db/models.py
+++ db/models.py
@@ -111,7 +111,8 @@ class Ticket(models.Model):
             raise ValidationError(
                 {
                     "row": f"row number must be in available range: "
-                           f"(1, rows): (1, {self.movie_session.cinema_hall.rows})"
+                           f"(1, rows): "
+                           f"(1, {self.movie_session.cinema_hall.rows})"
                 }
             )
 
@@ -121,7 +122,8 @@ class Ticket(models.Model):
                 {
                     "seat": f"seat number must be in available range: "
                             f"(1, seats_in_row): "
-                            f"(1, {self.movie_session.cinema_hall.seats_in_row})"
+                            f"(1, "
+                            f"{self.movie_session.cinema_hall.seats_in_row})"
                 }
             )
 
