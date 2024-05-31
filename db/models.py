ac827eb76d4697882a38d6c4d67db5620325e5b6
Yaryna Hryniv <ygrinsv@gmail.com>
2024-05-31 13:20:15 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,8 +1,8 @@
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import UniqueConstraint
+import settings
 
 
 class Genre(models.Model):
@@ -21,7 +21,7 @@ class Actor(models.Model):
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255, db_index=True)
     description = models.TextField()
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
@@ -33,6 +33,36 @@ class Movie(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class MovieSession(models.Model):
+    show_time = models.DateTimeField()
+    cinema_hall = models.ForeignKey(
+        to=CinemaHall,
+        on_delete=models.CASCADE,
+        related_name="movie_sessions"
+    )
+    movie = models.ForeignKey(
+        to=Movie,
+        on_delete=models.CASCADE,
+        related_name="movie_sessions"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.movie.title} {str(self.show_time)}"
 
 
 class Order(models.Model):
@@ -50,8 +80,12 @@ class Order(models.Model):
 
 
 class Ticket(models.Model):
-    movie_session = models.ForeignKey(to="MovieSession", on_delete=models.CASCADE)
-    order = models.ForeignKey(to="Order", on_delete=models.CASCADE)
+    movie_session = models.ForeignKey(
+        to="MovieSession",
+        on_delete=models.CASCADE)
+    order = models.ForeignKey(
+        to="Order",
+        on_delete=models.CASCADE)
     row = models.IntegerField()
     seat = models.IntegerField()
 
@@ -78,32 +112,6 @@ class Ticket(models.Model):
             UniqueConstraint(fields=["row", "seat", "movie_session"],
                              name="unique_ticket")
         ]
-
-
-class CinemaHall(models.Model):
-    name = models.CharField(max_length=255)
-    rows = models.IntegerField()
-    seats_in_row = models.IntegerField()
-
-    @property
-    def capacity(self) -> int:
-        return self.rows * self.seats_in_row
-
-    def __str__(self) -> str:
-        return self.name
-
-
-class MovieSession(models.Model):
-    show_time = models.DateTimeField()
-    cinema_hall = models.ForeignKey(
-        to=CinemaHall, on_delete=models.CASCADE, related_name="movie_sessions"
-    )
-    movie = models.ForeignKey(
-        to=Movie, on_delete=models.CASCADE, related_name="movie_sessions"
-    )
-
-    def __str__(self) -> str:
-        return f"{self.movie.title} {str(self.show_time)}"
 
 
 class User(AbstractUser):
