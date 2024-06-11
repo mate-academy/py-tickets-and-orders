7ee8a5fc2cf57193a198b1c7a759c2e31d50eaf9
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2024-06-11 11:36:14 +0300

    fix

--- services/order.py
+++ services/order.py
@@ -8,34 +8,32 @@ from django.db.models import QuerySet
 from db.models import Order, Ticket
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict[str, Any]],
         username: str,
         date: str = ""
 ) -> Order:
-    with transaction.atomic():
-        order = Order.objects.create(
-            user=get_user_model().objects.get(username=username),
+    order = Order.objects.create(
+        user=get_user_model().objects.get(username=username),
+    )
+    if date:
+        order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+
+    order.save()
+
+    for ticket in tickets:
+        Ticket.objects.create(
+            order=order,
+            row=ticket["row"],
+            seat=ticket["seat"],
+            movie_session_id=ticket["movie_session"],
         )
-        if date:
-            order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
 
-        order.save()
-
-        for ticket in tickets:
-            Ticket.objects.create(
-                order=order,
-                row=ticket["row"],
-                seat=ticket["seat"],
-                movie_session_id=ticket["movie_session"],
-            )
-
-        return order
+    return order
 
 
-def get_orders(
-        username: str = ""
-) -> QuerySet[Order]:
+def get_orders(username: str = "") -> QuerySet[Order]:
     queryset = Order.objects.all()
     if username:
         queryset = queryset.filter(user__username=username)
--- services/user.py
+++ services/user.py
@@ -19,9 +19,7 @@ def create_user(
     )
 
 
-def get_user(
-        user_id: int,
-) -> User:
+def get_user(user_id: int) -> User:
     return get_user_model().objects.get(pk=user_id)
 
 
@@ -33,7 +31,7 @@ def update_user(
         first_name: str = None,
         last_name: str = None,
 ) -> None:
-    user = get_user_model().objects.get(pk=user_id)
+    user = get_user(user_id)
     if username:
         user.username = username
     if password:
