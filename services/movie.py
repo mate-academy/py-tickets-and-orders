b4aa43ca08b9af47c634b19c47b4c0aeddf38210
Yuliia Dudych <juliadudych@gmail.com>
2024-06-13 21:38:11 +0300

    Change 'transaction.atomic' from context manager to decorator

--- services/movie.py
+++ services/movie.py
@@ -24,6 +24,7 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
+@transaction.atomic
 def create_movie(
     movie_title: str,
     movie_description: str,
@@ -31,14 +32,13 @@ def create_movie(
     actors_ids: list | None = None,
 ) -> Movie:
 
-    with transaction.atomic():
-        movie = Movie.objects.create(
-            title=movie_title,
-            description=movie_description,
-        )
-        if genres_ids:
-            movie.genres.set(genres_ids)
-        if actors_ids:
-            movie.actors.set(actors_ids)
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
 
-        return movie
+    return movie
--- services/order.py
+++ services/order.py
@@ -5,30 +5,31 @@ from django.db.models import QuerySet
 from db.models import Order, Ticket, MovieSession
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str | None = None
 ) -> None:
-    with transaction.atomic():
-        user = get_user_model().objects.get(username=username)
 
-        order = Order.objects.create(user=user)
-        if date:
-            order.created_at = date
-            order.save()
+    user = get_user_model().objects.get(username=username)
 
-        ticket_objects = []
-        for ticket in tickets:
-            ticket_objects.append(Ticket(
-                row=ticket["row"],
-                seat=ticket["seat"],
-                order=order,
-                movie_session=MovieSession.objects.get(
-                    pk=ticket["movie_session"]
-                ),
-            ))
-        Ticket.objects.bulk_create(ticket_objects)
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = date
+        order.save()
+
+    ticket_objects = []
+    for ticket in tickets:
+        ticket_objects.append(Ticket(
+            row=ticket["row"],
+            seat=ticket["seat"],
+            order=order,
+            movie_session=MovieSession.objects.get(
+                pk=ticket["movie_session"]
+            ),
+        ))
+    Ticket.objects.bulk_create(ticket_objects)
 
 
 def get_orders(username: str | None = None) -> QuerySet:
