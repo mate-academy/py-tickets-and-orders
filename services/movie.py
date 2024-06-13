df592d0b6b8ec9410198408c4ce0a4b26ceab4c0
Yuliia Dudych <juliadudych@gmail.com>
2024-06-13 18:54:32 +0300

    Add 'title' argument to 'get_movies' function

--- services/movie.py
+++ services/movie.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.db.models import QuerySet
 
 from db.models import Movie
@@ -6,16 +7,17 @@ from db.models import Movie
 def get_movies(
     genres_ids: list[int] = None,
     actors_ids: list[int] = None,
+    title: str = None,
 ) -> QuerySet:
-    queryset = Movie.objects.all()
+    movies = Movie.objects.all()
 
     if genres_ids:
-        queryset = queryset.filter(genres__id__in=genres_ids)
+        movies = movies.filter(genres__id__in=genres_ids)
 
     if actors_ids:
-        queryset = queryset.filter(actors__id__in=actors_ids)
+        movies = movies.filter(actors__id__in=actors_ids)
 
-    return queryset
+    return movies.filter(title__icontains=title) if title else movies
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
@@ -28,6 +30,7 @@ def create_movie(
     genres_ids: list = None,
     actors_ids: list = None,
 ) -> Movie:
+
     movie = Movie.objects.create(
         title=movie_title,
         description=movie_description,
