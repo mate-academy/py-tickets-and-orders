a1396c879fe8183e1412b0be80a05058d89f2e0f
Ntaviouk <dimasdaniuk@gmail.com>
2024-06-26 13:09:34 +0300

    some fixes

--- services/order.py
+++ services/order.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
 
-from db.models import Order, Ticket, User
+from db.models import Order, Ticket
 
 
 @transaction.atomic
