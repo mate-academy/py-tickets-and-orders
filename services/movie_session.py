055012e871e310654bae924aede3ababb243dae9
Dima Drozdenko <anonimchik920@gmail.com>
2024-06-27 23:53:03 +0300

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -24,6 +24,14 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    movie_session = MovieSession.objects.get(pk=movie_session_id).tickets.all()
+    return [
+        {"row": ticket.row, "seat": ticket.seat}
+        for ticket in movie_session
+    ]
+
+
 def update_movie_session(
     session_id: int,
     show_time: str = None,
---
+++ services/user.py
@@ -0,0 +1,50 @@
+from django.contrib.auth import get_user_model
+
+
+def create_user(
+        username: str,
+        password: str,
+        email: str | None = None,
+        first_name: str | None = None,
+        last_name: str | None = None
+) -> None:
+    user = get_user_model().objects.create_user(
+        username=username,
+        password=password
+    )
+
+    if email:
+        user.email = email
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
+
+    user.save()
+
+
+def get_user(user_id: str) -> None:
+    return get_user_model().objects.get(pk=user_id)
+
+
+def update_user(
+        user_id: str,
+        username: str | None = None,
+        password: str | None = None,
+        email: str | None = None,
+        first_name: str | None = None,
+        last_name: str | None = None
+) -> None:
+    user = get_user_model().objects.get(pk=user_id)
+    if username:
+        user.username = username
+    if password:
+        user.set_password(password)
+    if email:
+        user.email = email
+    if first_name:
+        user.first_name = first_name
+    if last_name:
+        user.last_name = last_name
+
+    user.save()
