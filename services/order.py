8fd6df518801606584c182d5998cc390a1d0b36c
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-06-29 13:49:24 +0300

    Solution atomic decorator

--- services/order.py
+++ services/order.py
@@ -5,24 +5,24 @@ from datetime import datetime
 from django.db.models import QuerySet
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = None
 ) -> None:
-    with transaction.atomic():
-        user = get_user_model().objects.get(username=username)
-        order = Order.objects.create(user=user)
-        if date:
-            order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
-        order.save()
-        for ticket in tickets:
-            Ticket.objects.create(
-                movie_session_id=ticket["movie_session"],
-                order=order,
-                row=ticket["row"],
-                seat=ticket["seat"]
-            )
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = datetime.strptime(date, "%Y-%m-%d %H:%M")
+    order.save()
+    for ticket in tickets:
+        Ticket.objects.create(
+            movie_session_id=ticket["movie_session"],
+            order=order,
+            row=ticket["row"],
+            seat=ticket["seat"]
+        )
 
 
 def get_orders(username: str = None) -> QuerySet[Order]:
