07d94c89c89189fb9362b7b762429558302e3833
dsokolovskiy <souldead147@gmail.com>
2024-07-01 12:01:03 +0300

    'Solution'

--- services/movie.py
+++ services/movie.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.db.models import QuerySet
 
 from db.models import Movie
@@ -6,16 +7,20 @@ from db.models import Movie
 def get_movies(
     genres_ids: list[int] = None,
     actors_ids: list[int] = None,
+    title: str = None
 ) -> QuerySet:
-    queryset = Movie.objects.all()
+    movies_queryset = Movie.objects.all()
 
     if genres_ids:
-        queryset = queryset.filter(genres__id__in=genres_ids)
+        movies_queryset = movies_queryset.filter(genres__id__in=genres_ids)
 
     if actors_ids:
-        queryset = queryset.filter(actors__id__in=actors_ids)
+        movies_queryset = movies_queryset.filter(actors__id__in=actors_ids)
 
-    return queryset
+    if title:
+        movies_queryset = movies_queryset.filter(title__icontains=title)
+
+    return movies_queryset
 
 
 def get_movie_by_id(movie_id: int) -> Movie:
@@ -28,13 +33,14 @@ def create_movie(
     genres_ids: list = None,
     actors_ids: list = None,
 ) -> Movie:
-    movie = Movie.objects.create(
-        title=movie_title,
-        description=movie_description,
-    )
-    if genres_ids:
-        movie.genres.set(genres_ids)
-    if actors_ids:
-        movie.actors.set(actors_ids)
+    with transaction.atomic():
+        movie = Movie.objects.create(
+            title=movie_title,
+            description=movie_description,
+        )
+        if genres_ids:
+            movie.genres.set(genres_ids)
+        if actors_ids:
+            movie.actors.set(actors_ids)
 
-    return movie
+        return movie
