b75288211a63c68f06b43df02b2c413c0ba107dd
Ivan <ivan.seeker6@gmail.com>
2024-07-01 17:45:19 +0300

    sol1

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ USE_TZ = False
 
 INSTALLED_APPS = [
     "db",
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
 ]
-AUTH_USER_MODEL = 'db.User'
+AUTH_USER_MODEL = "db.User"
