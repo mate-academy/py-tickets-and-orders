9decd0ca90c99c8dda7a08a0174dce2ef4a06535
lilarin <80650911+lilarin@users.noreply.github.com>
2024-07-07 16:05:39 +0300

    Solution refinement

--- db/models.py
+++ db/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -39,7 +40,7 @@ class User(AbstractUser):
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ["-created_at"]
