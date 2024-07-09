a772b480f5f87b24acbd44bffada180a9405801a
mykyta <nikita.fedotov222@gmail.com>
2024-07-09 17:39:53 +0300

    solution

--- services/order.py
+++ services/order.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import transaction
 
-from db.models import Order, Ticket, User, MovieSession
+from db.models import Order, Ticket, MovieSession
 
 
 def create_order(
@@ -16,11 +16,11 @@ def create_order(
             order.save()
         for ticket in tickets:
             movie_session = MovieSession.objects.get(
-                pk=ticket['movie_session']
+                pk=ticket["movie_session"]
             )
             ticket = Ticket.objects.create(
-                row=ticket['row'],
-                seat=ticket['seat'],
+                row=ticket["row"],
+                seat=ticket["seat"],
                 movie_session=movie_session,
                 order=order
             )
