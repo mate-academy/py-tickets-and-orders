5c372b572af04989a53f7a8c06b8f643c7f8a2de
mykyta <nikita.fedotov222@gmail.com>
2024-07-09 18:52:17 +0300

    solution

--- db/models.py
+++ db/models.py
@@ -65,7 +65,9 @@ class MovieSession(models.Model):
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders",
     )
 
     class Meta:
--- services/order.py
+++ services/order.py
@@ -4,29 +4,29 @@ from django.db import transaction
 from db.models import Order, Ticket, MovieSession
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = None) -> None:
-    with transaction.atomic():
-        user = get_user_model().objects.get(username=username)
-        order = Order.objects.create(user=user)
-        if date:
-            order.created_at = date
-            order.save()
-        for ticket in tickets:
-            movie_session = MovieSession.objects.get(
-                pk=ticket["movie_session"]
-            )
-            ticket = Ticket.objects.create(
-                row=ticket["row"],
-                seat=ticket["seat"],
-                movie_session=movie_session,
-                order=order
-            )
-            ticket.full_clean()
-            ticket.save()
-        return order
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = date
+        order.save()
+    for ticket in tickets:
+        movie_session = MovieSession.objects.get(
+            pk=ticket["movie_session"]
+        )
+        ticket = Ticket.objects.create(
+            row=ticket["row"],
+            seat=ticket["seat"],
+            movie_session=movie_session,
+            order=order
+        )
+        ticket.full_clean()
+        ticket.save()
+    return order
 
 
 def get_orders(username: str = None) -> list[Order]:
