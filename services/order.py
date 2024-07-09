43e7512479ecfa6a13f3f51bc4e7c52d12063cc7
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-07-09 19:05:50 +0300

    fixed mistake

--- services/order.py
+++ services/order.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
 
-from db.models import Order, Ticket
+from db.models import Order, Ticket, MovieSession
 
 
 @transaction.atomic
@@ -15,17 +15,20 @@ def create_order(
     order = Order.objects.create(user=user)
     if date:
         order.created_at = date
+    order.save()
     Ticket.objects.bulk_create(
         [
             Ticket(
                 row=ticket["row"],
                 seat=ticket["seat"],
-                movie_session_id=ticket["movie_session"],
+                movie_session=MovieSession.objects.get(
+                    id=ticket["movie_session"]
+                ),
                 order=order)
             for ticket in tickets
         ]
     )
-    order.save()
+
     return order
 
 
