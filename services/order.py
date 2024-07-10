1275895a26a197aed3d08dc21139734f8dd677bb
Anton Korinchuk <antonkorinchuk@gmail.com>
2024-07-10 14:18:25 +0300

    fixed func(create order)

--- services/order.py
+++ services/order.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
 
-from db.models import Order, Ticket, MovieSession
+from db.models import Order, Ticket
 
 
 @transaction.atomic
@@ -16,19 +16,15 @@ def create_order(
     if date:
         order.created_at = date
     order.save()
-    Ticket.objects.bulk_create(
-        [
-            Ticket(
-                row=ticket["row"],
-                seat=ticket["seat"],
-                movie_session=MovieSession.objects.get(
-                    id=ticket["movie_session"]
-                ),
-                order=order)
-            for ticket in tickets
-        ]
-    )
 
+    for ticket in tickets:
+        ticket = Ticket(
+            row=ticket["row"],
+            seat=ticket["seat"],
+            movie_session_id=ticket["movie_session"],
+            order=order
+        )
+        ticket.save()
     return order
 
 
