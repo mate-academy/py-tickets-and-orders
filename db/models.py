01aeece7d54f171beb083bce50176566c2e75139
Ira <iramladanovych@gmail.com>
2024-07-11 13:28:48 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -65,7 +65,8 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        related_name="orders",
     )
 
     class Meta:
@@ -84,7 +85,8 @@ class Ticket(models.Model):
         related_name="tickets"
     )
     order = models.ForeignKey(
-        Order, on_delete=models.CASCADE, related_name="tickets"
+        Order, on_delete=models.CASCADE,
+        related_name="tickets"
     )
 
     def __str__(self) -> str:
