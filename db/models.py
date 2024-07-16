dd04ee182d44e403e8968dba53d3b47a190fe913
Maksym Protsak <maksym.protsak@gmail.com>
2024-07-16 13:17:49 +0300

    Solution 16-07-2024 13-17

--- db/models.py
+++ db/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+from django.core.exceptions import ValidationError
 
 
 class Genre(models.Model):
@@ -55,3 +57,40 @@ class MovieSession(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey("User", on_delete=models.CASCADE,
+                             related_name="orders")  # NEED TO CHANGE TO THE USER FROM SETTINGS
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{Order.__name__}: {self.created_at}"
+
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey(to=MovieSession, on_delete=models.CASCADE, related_name="tickets")
+    order = models.ForeignKey(to=Order, on_delete=models.CASCADE, related_name="tickets")
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    class Meta:
+        constraints = UniqueConstraint(
+            fields=["movie_session", "row", "seat"], name="unique_movie_session_row_seat"
+        )
+
+    def __str__(self):
+        return f"{Ticket.__name__}: {self.movie_session.movie} {self.movie_session.show_time} (row: {self.row}, seat: {self.seat})"
+
+    def clean(self):
+        if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
+            raise ValidationError(f"row: row must be in range [1, {self.movie_session.cinema_hall.rows}], not {self.row}")
+        if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
+            raise ValidationError(f"seat: seat must be in range [1, {self.movie_session.cinema_hall.seats_in_row}], not {self.seat}")
+
+    def sava(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
