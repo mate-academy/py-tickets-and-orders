016e52f6ca3040093a86ec8d22db64317ffd7ce0
Maksym Protsak <maksym.protsak@gmail.com>
2024-07-17 21:55:49 +0300

    Solution 17-07-2024 21-55

--- services/order.py
+++ services/order.py
@@ -4,7 +4,7 @@ from db.models import Order, Ticket, MovieSession
 from django.contrib.auth import get_user_model
 
 
-def create_order(tickets: list, username: str, date: str = None):
+def create_order(tickets: list, username: str, date: str = None) -> None:
     user_for_order = get_user_model().objects.get(username=username)
     new_order = Order(user=user_for_order)
     if date:
@@ -24,3 +24,10 @@ def create_order(tickets: list, username: str, date: str = None):
         Ticket.objects.bulk_create(
             new_tickets
         )
+
+
+def get_orders(username: str = None) -> Order:
+    if username:
+        user = get_user_model().objects.get(username=username)
+        return Order.objects.filter(user_id=user.id)
+    return Order.objects.all().values()
