4e49322e87a41859fa9fc7a06cb5040c29e33973
Maksym Protsak <maksym.protsak@gmail.com>
2024-07-18 09:05:24 +0300

    Solution 18-07-2024 9-05

--- db/models.py
+++ db/models.py
@@ -107,15 +107,14 @@ class Ticket(models.Model):
             )
         if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
             raise ValidationError(
-                {
-                    "seat": f"seat number must be in available range: "
-                            f"(1, seats_in_row): "
-                            f"(1, {
+                dict(seat=f"seat number must be in available range: "
+                          f"(1, seats_in_row): "
+                          f"(1, {
                                 self.movie_session.cinema_hall.seats_in_row
-                            })"}
+                          })")
             )
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs) ->:
         self.full_clean()
         return super().save(*args, **kwargs)
 
