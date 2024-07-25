b259eecd7136a788701b4414e9a57368cb1ed75e
Viva1-ua <veronikaboeva935@gmail.com>
2024-07-25 09:52:49 +0200

    Solution 25/07/24

--- services/order.py
+++ services/order.py
@@ -9,21 +9,21 @@ def create_order(
         username: str,
         date: str = None
 ) -> None:
-        user = get_user_model().objects.get(username=username)
-        order = Order.objects.create(user=user)
+    user = get_user_model().objects.get(username=username)
+    order = Order.objects.create(user=user)
 
-        if date:
-            order.created_at = date
-            order.save()
-
-        for ticket in tickets:
-            Ticket.objects.create(
-                movie_session_id=ticket["movie_session"], order=order,
-                row=ticket["row"], seat=ticket["seat"]
-            )
-
+    if date:
+        order.created_at = date
         order.save()
-        return order
+
+    for ticket in tickets:
+        Ticket.objects.create(
+            movie_session_id=ticket["movie_session"], order=order,
+            row=ticket["row"], seat=ticket["seat"]
+        )
+
+    order.save()
+    return order
 
 
 def get_orders(username: str = None) -> None:
