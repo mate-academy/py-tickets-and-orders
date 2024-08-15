37adc26b5f6236ada275f117add40c9e98bab1c4
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-08-16 00:11:53 +0300

    "solution"

--- services/order.py
+++ services/order.py
@@ -6,24 +6,25 @@ from django.utils import timezone
 from db.models import Ticket, Order, User, MovieSession
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict], username: str, date: datetime = None
 ) -> Order:
-    with transaction.atomic():
-        user = User.objects.get(username=username)
-        order = Order.objects.create(
-            user=user, created_at=date or timezone.now()
+
+    user = User.objects.get(username=username)
+    order = Order.objects.create(
+        user=user, created_at=date or timezone.now()
+    )
+    for ticket in tickets:
+        Ticket.objects.create(
+            movie_session=MovieSession.objects.get(
+                id=ticket["movie_session"]
+            ),
+            order=order,
+            row=ticket["row"],
+            seat=ticket["seat"],
         )
-        for ticket in tickets:
-            Ticket.objects.create(
-                movie_session=MovieSession.objects.get(
-                    id=ticket["movie_session"]
-                ),
-                order=order,
-                row=ticket["row"],
-                seat=ticket["seat"],
-            )
-        return order
+    return order
 
 
 def get_orders(username: str = None) -> list[Order]:
