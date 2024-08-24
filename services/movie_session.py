bfcd205330aeb9a18db44b124249737167ad50f9
viktor-77 <vumanskiy0664041341@gmail.com>
2024-08-24 12:50:03 +0300

    Refactored Tickets and orders

--- services/movie_session.py
+++ services/movie_session.py
@@ -44,6 +44,9 @@ def delete_movie_session_by_id(session_id: int) -> None:
 
 
 def get_taken_seats(movie_session_id: int) -> list[dict]:
-    tickets = Ticket.objects.filter(movie_session_id=movie_session_id)
-
-    return [{"row": ticket.row, "seat": ticket.seat} for ticket in tickets]
+    return list(
+        Ticket.objects.filter(
+            movie_session_id=movie_session_id
+        )
+        .values("row", "seat")
+    )
--- services/order.py
+++ services/order.py
@@ -5,6 +5,7 @@ from django.db.models import QuerySet
 from db.models import Order, Ticket
 
 
+@transaction.atomic
 def create_order(
     tickets: list[dict],
     username: str,
@@ -12,21 +13,20 @@ def create_order(
 ) -> None:
     user = get_user_model().objects.get(username=username)
 
-    with transaction.atomic():
-        order = Order.objects.create(
-            user=user,
-        )
-        if date:
-            order.created_at = date
-        order.save()
+    order = Order.objects.create(
+        user=user,
+    )
+    if date:
+        order.created_at = date
+    order.save()
 
-        for ticket_data in tickets:
-            Ticket.objects.create(
-                order=order,
-                row=ticket_data["row"],
-                seat=ticket_data["seat"],
-                movie_session_id=ticket_data["movie_session"]
-            )
+    for ticket_data in tickets:
+        Ticket.objects.create(
+            order=order,
+            row=ticket_data["row"],
+            seat=ticket_data["seat"],
+            movie_session_id=ticket_data["movie_session"]
+        )
 
 
 def get_orders(username: str = None) -> QuerySet[Order]:
