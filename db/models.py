43cc94c0b0fd7a2638d78fbdf98d7385b53690c5
DSahalatyi <dsahalatyi@gmail.com>
2024-08-26 15:57:14 +0300

    fix linter error

--- db/models.py
+++ db/models.py
@@ -125,8 +125,9 @@ class Ticket(models.Model):
     def __str__(self) -> str:
         return (
             f"{self.movie_session.movie.title} "
-            f"{datetime.strftime(self.movie_session.show_time,
-                                 '%Y-%m-%d %H:%M:%S')} "
+            f"{datetime.strftime(
+                self.movie_session.show_time, '%Y-%m-%d %H:%M:%S'
+            )} "
             f"(row: {self.row}, seat: {self.seat})"
         )
 
