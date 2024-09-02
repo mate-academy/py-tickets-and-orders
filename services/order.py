69d6b4b24f3451f623c6a3413d26f7758bafca33
Andrii <andrii.khamaza0@gmail.com>
2024-09-02 22:33:53 +0300

    Solution

--- services/order.py
+++ services/order.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.db.models import QuerySet
 
-from db.models import Ticket, Order
+from db.models import Ticket, Order, MovieSession
 
 
 def create_order(tickets: list[dict], username: str, date: str = None) -> None:
@@ -14,15 +14,17 @@ def create_order(tickets: list[dict], username: str, date: str = None) -> None:
             order.created_at = date
             order.save()
 
-        ticket_objects = [
-            Ticket(
+        ticket_objects = []
+        for ticket in tickets:
+            movie_session = MovieSession.objects.get(
+                pk=ticket["movie_session"]
+            )
+            ticket_objects.append(Ticket(
                 order=order,
                 row=ticket["row"],
                 seat=ticket["seat"],
-                movie_session_id=ticket["movie_session"]
-            )
-            for ticket in tickets
-        ]
+                movie_session=movie_session
+            ))
 
         Ticket.objects.bulk_create(ticket_objects)
 
