218ce5cc8c92b2179cf20f35593656e7fe10c4f1
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2024-09-03 15:30:32 +0300

    The same relates here as well (use @transaction.atomic)

--- services/order.py
+++ services/order.py
@@ -4,26 +4,26 @@ from django.utils.dateparse import parse_datetime
 from db.models import Order, User, Ticket
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = None
 ) -> None:
-    with transaction.atomic():
-        user = User.objects.get(username=username)
-        order = Order.objects.create(user=user)
-        if date:
-            parsed_date = parse_datetime(date)
-            order.created_at = parsed_date
-            order.save()
+    user = User.objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        parsed_date = parse_datetime(date)
+        order.created_at = parsed_date
+        order.save()
 
-        for ticket in tickets:
-            Ticket.objects.create(
-                order=order,
-                row=ticket["row"],
-                seat=ticket["seat"],
-                movie_session_id=ticket["movie_session"]
-            )
+    for ticket in tickets:
+        Ticket.objects.create(
+            order=order,
+            row=ticket["row"],
+            seat=ticket["seat"],
+            movie_session_id=ticket["movie_session"]
+        )
 
 
 def get_orders(username: str = None) -> list[Order]:
