1fec026f095e87b0b58f6f508a26c84c62e2dd80
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-09-03 11:24:29 +0300

    solving

---
+++ services/order.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Order, Ticket
+
+
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: str | None = None
+) -> None:
+    user = get_user_model().objects.get(username=username)
+    new_data = {}
+    if date:
+        new_data["created_at"] = date
+
+    with transaction.atomic():
+        order = Order.objects.create(user=user)
+        Order.objects.filter(id=order.id).update(**new_data)
+        for ticket in tickets:
+            ticket["movie_session_id"] = ticket["movie_session"]
+            del ticket["movie_session"]
+            Ticket.objects.create(order=order, **ticket)
+
+
+def get_orders(username: str | None = None) -> QuerySet:
+    kwargs = {}
+    if username:
+        kwargs["user"] = get_user_model().objects.get(username=username)
+    return Order.objects.filter(**kwargs)
