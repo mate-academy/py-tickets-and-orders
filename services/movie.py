c9990ecc086f2300814c3bf11e3d5b69aa66e81c
Misha Havryliuk <misharob31415@gmail.com>
2024-09-05 09:11:56 +0300

    Solution2

--- services/movie.py
+++ services/movie.py
@@ -5,10 +5,10 @@ from db.models import Movie
 
 
 def get_movies(
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None,
-    title: str = None,
-) -> QuerySet:
+    genres_ids: list[int] | None = None,
+    actors_ids: list[int] | None = None,
+    title: str | None = None,
+) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
 
     if genres_ids:
@@ -30,8 +30,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 def create_movie(
     movie_title: str,
     movie_description: str,
-    genres_ids: list = None,
-    actors_ids: list = None,
+    genres_ids: list | None = None,
+    actors_ids: list | None = None,
 ) -> Movie:
     with transaction.atomic():
         movie = Movie.objects.create(
--- services/movie_session.py
+++ services/movie_session.py
@@ -13,7 +13,9 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet:
+def get_movies_sessions(
+        session_date: str | None = None
+) -> QuerySet[MovieSession]:
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
@@ -26,9 +28,9 @@ def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
 
 def update_movie_session(
     session_id: int,
-    show_time: str = None,
-    movie_id: int = None,
-    cinema_hall_id: int = None,
+    show_time: str | None = None,
+    movie_id: int | None = None,
+    cinema_hall_id: int | None = None,
 ) -> None:
     movie_session = MovieSession.objects.get(id=session_id)
     if show_time:
@@ -45,5 +47,8 @@ def delete_movie_session_by_id(session_id: int) -> None:
 
 
 def get_taken_seats(movie_session_id: int) -> list[dict]:
-    tickets = Ticket.objects.filter(movie_session_id=movie_session_id)
-    return [{"row": ticket.row, "seat": ticket.seat} for ticket in tickets]
+    return list(
+        Ticket.objects
+        .filter(movie_session_id=movie_session_id)
+        .values("row", "seat")
+    )
