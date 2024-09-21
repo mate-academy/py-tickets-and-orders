f66ac412684466c959481afa267aecd41c8a9b1e
Frezworx <frezworx@gmail.com>
2024-09-21 19:58:46 +0300

    applied atomic transactions execution to some functions

--- services/movie.py
+++ services/movie.py
@@ -4,24 +4,43 @@ from django.db.models import QuerySet
 from db.models import Movie
 
 
+@transaction.atomic()
+def create_movie(
+        movie_title: str,
+        movie_description: str,
+        genres_ids: list = None,
+        actors_ids: list = None,
+) -> Movie:
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
+
+    return movie
+
+
 def get_movies(
         genres_ids: list[int] = None,
         actors_ids: list[int] = None,
         title: str = None
 ) -> QuerySet:
-    with transaction.atomic():
-        queryset = Movie.objects.all()
 
-        if genres_ids:
-            queryset = queryset.filter(genres__id__in=genres_ids)
+    queryset = Movie.objects.all()
 
-        if actors_ids:
-            queryset = queryset.filter(actors__id__in=actors_ids)
+    if genres_ids:
+        queryset = queryset.filter(genres__id__in=genres_ids)
 
-        if title:
-            queryset = queryset.filter(
-                title__icontains=title
-            ).values_list("title")
+    if actors_ids:
+        queryset = queryset.filter(actors__id__in=actors_ids)
+
+    if title:
+        queryset = queryset.filter(
+            title__icontains=title
+        ).values_list("title")
 
     return queryset
 
@@ -30,20 +49,20 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
+@transaction.atomic
 def create_movie(
         movie_title: str,
         movie_description: str,
-        genres_ids: list = None,
-        actors_ids: list = None,
+        genres_ids: list[int] = None,
+        actors_ids: list[int] = None,
 ) -> Movie:
-    with transaction.atomic():
-        movie = Movie.objects.create(
-            title=movie_title,
-            description=movie_description,
-        )
-        if genres_ids:
-            movie.genres.set(genres_ids)
-        if actors_ids:
-            movie.actors.set(actors_ids)
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
 
     return movie
--- services/order.py
+++ services/order.py
@@ -4,41 +4,41 @@ from django.db.models import QuerySet
 from db.models import Order, User, Ticket, MovieSession
 
 
+@transaction.atomic
 def create_order(
         tickets: list[dict],
         username: str,
         date: str = None
 ) -> Order:
-    with transaction.atomic():
-        user = User.objects.get(username=username)
-        order = Order.objects.create(user=user)
-        if date:
-            order.created_at = date
 
-        order.save()
+    user = User.objects.get(username=username)
+    order = Order.objects.create(user=user)
+    if date:
+        order.created_at = date
 
-        tickets_obj = [
-            Ticket(
-                order=order,
-                movie_session=MovieSession.objects.get(
-                    pk=ticket["movie_session"]
-                ),
-                row=ticket["row"],
-                seat=ticket["seat"]
-            )
-            for ticket in tickets
-        ]
+    order.save()
 
-        Ticket.objects.bulk_create(tickets_obj)
+    tickets_obj = [
+        Ticket(
+            order=order,
+            movie_session=MovieSession.objects.get(
+                pk=ticket["movie_session"]
+            ),
+            row=ticket["row"],
+            seat=ticket["seat"]
+        )
+        for ticket in tickets
+    ]
 
-        return order
+    Ticket.objects.bulk_create(tickets_obj)
+
+    return order
 
 
+@transaction.atomic
 def get_orders(username: str = None) -> QuerySet:
-    with transaction.atomic():
-        orders = Order.objects.all()
-
-        if username:
-            orders = Order.objects.filter(user__username=username)
+    orders = Order.objects.all()
+    if username:
+        orders = Order.objects.filter(user__username=username)
 
     return orders
