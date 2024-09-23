05c29621e6fcd2de133631cfc0df362417c1e6ea
Frezworx <frezworx@gmail.com>
2024-09-23 14:47:45 +0300

    apply transaction.atomic() as a function decorator

--- services/movie_session.py
+++ services/movie_session.py
@@ -41,7 +41,7 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> None:
-    MovieSession.objects.filter(id=session_id).delete()
+    get_movie_session_by_id(session_id).delete()
 
 
 def get_taken_seats(movie_session_id: int) -> list[dict]:
