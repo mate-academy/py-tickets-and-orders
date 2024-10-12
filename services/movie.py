8d1dd9be0eec63768bd5432082b1268d6bf433be
Viacheslav417865 <sfinks417865@gmail.com>
2024-10-12 17:26:51 +0300

    Solution

--- services/movie.py
+++ services/movie.py
@@ -1,12 +1,12 @@
-from typing import Any
+from typing import Any, Optional, List
 from django.db import transaction
 from db.models import Movie
 
 
 def get_movie(
-    title: str | None = None,
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None,
+    title: Optional[str] = None,
+    genres_ids: Optional[List[int]] = None,
+    actors_ids: Optional[List[int]] = None,
 ) -> Any:
     queryset = Movie.objects.all()
 
@@ -26,22 +26,27 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
-def create_movie(movie_title: Any, movie_description: Any,
-                 genres_ids: Any, actors_ids: Any) -> Movie:
-    try:
-        with ((((transaction.atomic())))):
-            if not isinstance(genres_ids, list) or not all(
-                    isinstance(i, int) for i in genres_ids):
-                raise ValueError("Genres should be a list of integers")
+def create_movie(
+    movie_title: str,
+    movie_description: str,
+    genres_ids: List[int],
+    actors_ids: List[int]
+) -> Movie:
+    with transaction.atomic():
+        if not isinstance(genres_ids, list) or not all(isinstance(
+                i, int) for i in genres_ids):
+            raise ValueError("Genres should be a list of integers")
 
-            movie = Movie.objects.create(
-                title=movie_title,
-                description=movie_description
-            )
+        if not isinstance(actors_ids, list) or not all(isinstance(
+                i, int) for i in actors_ids):
+            raise ValueError("Actors should be a list of integers")
 
-            movie.genres.set(genres_ids)
-            movie.actors.set(actors_ids)
+        movie = Movie.objects.create(
+            title=movie_title,
+            description=movie_description
+        )
 
-            return movie
-    except Exception as e:
-        raise e
+        movie.genres.set(genres_ids)
+        movie.actors.set(actors_ids)
+
+        return movie
